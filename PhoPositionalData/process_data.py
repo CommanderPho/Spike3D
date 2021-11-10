d685af0b575e1297aafcf74979b2e05329513423
Pho Hale <PhoHale@gmail.com>
2021-11-10 16:14:07 -0500

    working

--- PhoPositionalData/process_data.py
+++ PhoPositionalData/process_data.py
@@ -54,27 +54,40 @@ def bin_edges_to_midpoints(x):
     return (x[1:] + x[:-1]) / 2
 
 
-def process_chunk_equal_poritions_data(t, x, y, speeds, dt, dx, dy, curr_view_window_length=30):
-    # Split the position data into equal sized chunks to be displayed at a single time. These will look like portions of the trajectory and be used to animate. # Chunk the data to create the animation.
+def process_chunk_equal_poritions_data_vectors(data_vectors_matrix, curr_view_window_length=30):
+    # data_vectors_matrix: a N x M matrix where M is the number of equal length data vectors (of length N)
+    num_data_vectors = np.shape(data_vectors_matrix)[0]
+    data_vector_length = np.shape(data_vectors_matrix)[1]
+     # Split the position data into equal sized chunks to be displayed at a single time. These will look like portions of the trajectory and be used to animate. # Chunk the data to create the animation.
     # curr_view_window_length = 150 # View 5 seconds at a time (30fps)
     # curr_view_window_length = 30 # View 5 seconds at a time (30fps)
     # The original length 324574 / 30 = 10819
-    trimmed_elements = np.remainder(np.size(x), curr_view_window_length) # Compute the number of elements that need to be droppped to be able to evently divide the original arrays into evenly sized chunks of length `curr_view_window_length`
+    trimmed_elements = np.remainder(data_vector_length, curr_view_window_length) # Compute the number of elements that need to be droppped to be able to evently divide the original arrays into evenly sized chunks of length `curr_view_window_length`
     # e.g. np.remainder(324574, 150) = 124
     # drop 124 extra elements that make it no wrap evenly
-    trimmed_length = np.size(x) - trimmed_elements
+    trimmed_length = data_vector_length - trimmed_elements
     # e.g. trimmed_length = 324574 - 124 # 324574 - 124
-    other_reshaped_dimension = np.floor_divide(np.size(x), curr_view_window_length) # e.g. 2163
+    other_reshaped_dimension = np.floor_divide(data_vector_length, curr_view_window_length) # e.g. 2163
 
-    t_fixedSegements = t[0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
-    x_fixedSegements = x[0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
-    y_fixedSegements = y[0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
+    reshaped_data_matrix = np.zeros((num_data_vectors, other_reshaped_dimension, curr_view_window_length))
+    for i in np.arange(num_data_vectors):
+        reshaped_data_matrix[i,:,:] = data_vectors_matrix[i, 0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
 
-    speeds_fixedSegements = speeds[0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
-    dt_fixedSegements = dt[0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
-    dx_fixedSegements = dx[0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
-    dy_fixedSegements = dy[0:trimmed_length].reshape(other_reshaped_dimension, curr_view_window_length)
+    return reshaped_data_matrix
 
+
+
+
+def process_chunk_equal_poritions_data(t, x, y, speeds, dt, dx, dy, curr_view_window_length=30):
+    # Split the position data into equal sized chunks to be displayed at a single time. These will look like portions of the trajectory and be used to animate. # Chunk the data to create the animation.
+    # wraps process_chunk_equal_poritions_data_vectors(...) for a fixed input/output list.
+    # data_vectors_matrix = np.vstack([t, x, y, speeds, dt, dx, dy]) # pack the variables of interest into the data_vector_matrix
+    outputMatrix_fixedSegements = process_chunk_equal_poritions_data_vectors(np.vstack([t, x, y, speeds, dt, dx, dy]), curr_view_window_length)
+    # print('shape - outputMatrix_fixedSegements: {}'.format(np.shape(outputMatrix_fixedSegements)))
+    num_data_vectors = np.shape(outputMatrix_fixedSegements)[0]
+    # unpack the result
+    t_fixedSegements,x_fixedSegements,y_fixedSegements,speeds_fixedSegements,dt_fixedSegements,dx_fixedSegements,dy_fixedSegements = [np.squeeze(outputMatrix_fixedSegements[i,:,:]) for i in np.arange(num_data_vectors)]
+    # print('shapes - t_fixedSegements: {}, x_fixedSegements: {}, y_fixedSegements: {}'.format(np.shape(t_fixedSegements), np.shape(x_fixedSegements), np.shape(y_fixedSegements)))
     return t_fixedSegements,x_fixedSegements,y_fixedSegements,speeds_fixedSegements,dt_fixedSegements,dx_fixedSegements,dy_fixedSegements
 
 def extract_spike_timeseries(spike_cell):
