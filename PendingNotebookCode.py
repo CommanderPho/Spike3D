0b404c989b0c9caf0d362fcd264b7fd8e43925a7
Pho Hale <PhoHale@gmail.com>
2021-12-07 17:42:30 -0500

    working

--- PendingNotebookCode.py
+++ PendingNotebookCode.py
@@ -1,7 +1,10 @@
 ## This file serves as overflow from active Jupyter-lab notebooks, to eventually be refactored.
 
+from matplotlib.colors import ListedColormap
 import numpy as np
 import pandas as pd
+import pyvista as pv
+import pyvistaqt as pvqt # conda install -c conda-forge pyvistaqt
 
 from PhoPositionalData.analysis.interactive_placeCell_config import InteractivePlaceCellConfig, VideoOutputModeConfig, PlottingConfig, PlacefieldComputationParameters  # VideoOutputModeConfig, InteractivePlaceCellConfigs
 from PhoPositionalData.analysis.interactive_placeCell_config import print_subsession_neuron_differences
@@ -31,7 +34,7 @@ def compute_placefields_as_needed(active_session, computation_config=None, activ
     active_epoch_placefields1D, active_epoch_placefields2D = perform_compute_placefields(active_session.neurons, active_session.position, computation_config, active_epoch_placefields1D, active_epoch_placefields2D, should_force_recompute_placefields=True)
     # Plot the placefields computed and save them out to files:
     if should_display_2D_plots:
-        ax_pf_1D, occupancy_fig, active_pf_2D_figures = plot_all_placefields(active_epoch_placefields1D, active_epoch_placefields2D, active_config)
+        ax_pf_1D, occupancy_fig, active_pf_2D_figures = plot_all_placefields(active_epoch_placefields1D, active_epoch_placefields2D, active_session.config)
     else:
         print('skipping 2D placefield plots')
     return active_epoch_placefields1D, active_epoch_placefields2D
@@ -51,7 +54,7 @@ def process_by_good_placefields(session, active_config, active_placefields):
     print('good_placefield_neuronIDs: {}; ({} good)'.format(good_placefield_neuronIDs, len(good_placefield_neuronIDs)))
 
     ## Filter by neurons with good placefields only:
-    good_placefields_session = active_epoch_session.get_by_id(good_placefield_neuronIDs) # Filter by good placefields only, and this fetch also ensures they're returned in the order of sorted ascending index ([ 2  3  5  7  9 12 18 21 22 23 26 27 29 34 38 45 48 53 57])
+    good_placefields_session = session.get_by_id(good_placefield_neuronIDs) # Filter by good placefields only, and this fetch also ensures they're returned in the order of sorted ascending index ([ 2  3  5  7  9 12 18 21 22 23 26 27 29 34 38 45 48 53 57])
 
     pf_sort_ind, pf_colors, pf_colormap, pf_listed_colormap = build_units_colormap(session)
     active_config.plotting_config.pf_sort_ind = pf_sort_ind
@@ -61,18 +64,41 @@ def process_by_good_placefields(session, active_config, active_placefields):
     
     return good_placefields_session, active_config, good_placefield_neuronIDs
 
-
 ## For building the configs used to filter the session by epoch: 
 def build_configs(session_config, active_epoch, active_subplots_shape = (1,1)):
     ## Get the config corresponding to this epoch/session settings:
     active_config = InteractivePlaceCellConfig(active_session_config=session_config, active_epochs=active_epoch, video_output_config=None, plotting_config=None) # '3|1    
 
     active_config.video_output_config = VideoOutputModeConfig(active_frame_range=np.arange(100.0, 120.0), 
-                                                              video_output_parent_dir=Path('output', active_sess_config.session_name, active_epoch.name),
+                                                              video_output_parent_dir=Path('output', session_config.session_name, active_epoch.name),
                                                               active_is_video_output_mode=False)
     active_config.plotting_config = PlottingConfig(output_subplots_shape=active_subplots_shape,
-                                                   output_parent_dir=Path('output', active_sess_config.session_name, active_epoch.name)
+                                                   output_parent_dir=Path('output', session_config.session_name, active_epoch.name)
                                                   )
     # Make the directories:
     active_config.plotting_config.active_output_parent_dir.mkdir(parents=True, exist_ok=True) # makes the directory if it isn't already there
     return active_config
+
+
+def build_placefield_multiplotter(nfields, linear_plot_data=None):
+    linear_plotter_indicies = np.arange(nfields)
+    fixed_columns = 5
+    needed_rows = int(np.ceil(nfields / fixed_columns))
+    row_column_indicies = np.unravel_index(linear_plotter_indicies, (needed_rows, fixed_columns)) # inverse is: np.ravel_multi_index(row_column_indicies, (needed_rows, fixed_columns))
+    mp = pvqt.MultiPlotter(nrows=needed_rows, ncols=fixed_columns, show=False, title='Muliplotter', toolbar=False, menu_bar=False, editor=False)
+    print('linear_plotter_indicies: {}\n row_column_indicies: {}\n'.format(linear_plotter_indicies, row_column_indicies))
+    # mp[0, 0].add_mesh(pv.Sphere())
+    # mp[0, 1].add_mesh(pv.Cylinder())
+    # mp[1, 0].add_mesh(pv.Cube())
+    # mp[1, 1].add_mesh(pv.Cone())
+    for a_linear_index in linear_plotter_indicies:
+        print('a_linear_index: {}, row_column_indicies[0][a_linear_index]: {}, row_column_indicies[1][a_linear_index]: {}'.format(a_linear_index, row_column_indicies[0][a_linear_index], row_column_indicies[1][a_linear_index]))
+        curr_row = row_column_indicies[0][a_linear_index]
+        curr_col = row_column_indicies[1][a_linear_index]
+        if linear_plot_data is None:
+            mp[curr_row, curr_col].add_mesh(pv.Sphere())
+        else:
+            mp[curr_row, curr_col].add_mesh(linear_plot_data[a_linear_index], name='maze_bg', color="black", render=False)
+            # mp[a_row_column_index[0], a_row_column_index[1]].add_mesh(pv.Sphere())
+    return mp, linear_plotter_indicies, row_column_indicies
+
--- PhoGui/InteractivePlotter/LapsVisualizationMixin.py
+++ PhoGui/InteractivePlotter/LapsVisualizationMixin.py
@@ -7,81 +7,75 @@ class LapsVisualizationMixin:
 
     @staticmethod
     def lines_from_points(points):
-		"""Given an array of points, make a line set"""
-		poly = pv.PolyData()
-		poly.points = points
-		cells = np.full((len(points)-1, 3), 2, dtype=np.int_)
-		cells[:, 1] = np.arange(0, len(points)-1, dtype=np.int_)
-		cells[:, 2] = np.arange(1, len(points), dtype=np.int_)
-		poly.lines = cells
-		return poly
+        """Given an array of points, make a line set"""
+        poly = pv.PolyData()
+        poly.points = points
+        cells = np.full((len(points)-1, 3), 2, dtype=np.int_)
+        cells[:, 1] = np.arange(0, len(points)-1, dtype=np.int_)
+        cells[:, 2] = np.arange(1, len(points), dtype=np.int_)
+        poly.lines = cells
+        return poly
+
+    @staticmethod
+    def get_lap_times(session, curr_lap_id):
+        return session.laps.get_lap_times(curr_lap_id)
+
+    @staticmethod
+    def get_lap_position(session, curr_lap_id):
+        curr_position_df = session.position.to_dataframe()
+        curr_lap_t_start, curr_lap_t_stop = session.laps.get_lap_times(curr_lap_id)
+        print('lap[{}]: ({}, {}): '.format(curr_lap_id, curr_lap_t_start, curr_lap_t_stop))
+
+        curr_lap_position_df_is_included = curr_position_df['t'].between(curr_lap_t_start, curr_lap_t_stop, inclusive=True) # returns a boolean array indicating inclusion in teh current lap
+        curr_lap_position_df = curr_position_df[curr_lap_position_df_is_included] 
+        # curr_position_df.query('-0.5 <= t < 0.5')
+        curr_lap_position_traces = curr_lap_position_df[['x','y']].to_numpy().T
+        print('\t {} positions.'.format(np.shape(curr_lap_position_traces)))
+        # print('\t {} spikes.'.format(curr_lap_num_spikes))
+        return curr_lap_position_traces
 
 
-	@staticmethod
-	def get_lap_times(session, curr_lap_id):
-		return session.laps.get_lap_times(curr_lap_id)
-
-	@staticmethod
-	def get_lap_position(session, curr_lap_id):
-		curr_position_df = session.position.to_dataframe()
-		curr_lap_t_start, curr_lap_t_stop = get_lap_times(curr_lap_id)
-		print('lap[{}]: ({}, {}): '.format(curr_lap_id, curr_lap_t_start, curr_lap_t_stop))
-
-		curr_lap_position_df_is_included = curr_position_df['t'].between(curr_lap_t_start, curr_lap_t_stop, inclusive=True) # returns a boolean array indicating inclusion in teh current lap
-		curr_lap_position_df = curr_position_df[curr_lap_position_df_is_included] 
-		# curr_position_df.query('-0.5 <= t < 0.5')
-		curr_lap_position_traces = curr_lap_position_df[['x','y']].to_numpy().T
-		print('\t {} positions.'.format(np.shape(curr_lap_position_traces)))
-		# print('\t {} spikes.'.format(curr_lap_num_spikes))
-		return curr_lap_position_traces
+    @staticmethod
+    def plot_lap_trajectory_path(interactiveDataExplorer, curr_lap_position_traces):
+        num_lap_samples = np.shape(curr_lap_position_traces)[1]
+        lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9)
+        plot_name = 'lap_location_trail'
+        trail_fade_values = None
+        size_values = None
+        trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
+        size_values = np.linspace(0.7, 0.3, num_lap_samples) # fade from a scale of 0.2 to 0.6
+        interactiveDataExplorer.perform_plot_location_trail(plot_name, curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z,
+                                                    trail_fade_values=trail_fade_values, trail_point_size_values=size_values,
+                                                    render=True, color='red')
 
 
-	@staticmethod
-	def plot_lap_trajectory_path(interactiveDataExplorer, curr_lap_position_traces):
-		num_lap_samples = np.shape(curr_lap_position_traces)[1]
-		lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9)
-		plot_name = 'lap_location_trail'
-		fade_values = interactiveDataExplorer.params.active_trail_opacity_values
-		# size_values = ipspikesDataExplorer.params.active_trail_size_values
-		trail_fade_values = None
-		size_values = None
-		trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
-		size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
-		interactiveDataExplorer.perform_plot_location_trail(plot_name, curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z,
-													trail_fade_values=trail_fade_values, trail_point_size_values=size_values,
-													render=True, color='red')
+    
+    @staticmethod
+    def plot_lap_trajectory_path_spline(p, curr_lap_position_traces):
+        num_lap_samples = np.shape(curr_lap_position_traces)[1]
+        lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9)
+        curr_lap_points = np.column_stack((curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z))
+        plot_name = 'lap_location_trail_spline'
+        trail_fade_values = None
+        size_values = None
+        trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
+        size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
+        line = LapsVisualizationMixin.lines_from_points(curr_lap_points)
+        line["scalars"] = np.arange(line.n_points)
+        tube = line.tube(radius=0.5)
+        p.add_mesh(tube, name=plot_name, render_lines_as_tubes=True, show_scalar_bar=False, color='red')
 
-
-	
-	@staticmethod
-	def plot_lap_trajectory_path_spline(interactiveDataExplorer, curr_lap_position_traces):
-		num_lap_samples = np.shape(curr_lap_position_traces)[1]
-		lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9)
-		curr_lap_points = np.column_stack((curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z))
-		plot_name = 'lap_location_trail_spline'
-		fade_values = interactiveDataExplorer.params.active_trail_opacity_values
-		# size_values = ipspikesDataExplorer.params.active_trail_size_values
-		trail_fade_values = None
-		size_values = None
-		trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
-		size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
-		line = LapsVisualizationMixin.lines_from_points(curr_lap_points)
-		line["scalars"] = np.arange(line.n_points)
-		tube = line.tube(radius=0.5)
-		# tube.plot(smooth_shading=True)
-		interactiveDataExplorer.p.add_mesh(tube, name=plot_name, render_lines_as_tubes=True, show_scalar_bar=False, color='red')
   
-  
-	@staticmethod
-	def plot_lap_trajectory_path_spline(interactiveDataExplorer, curr_lap_position_traces, curr_lap_id):
-		num_lap_samples = np.shape(curr_lap_position_traces)[1]
-		lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9 + (0.45 * curr_lap_id))
-		curr_lap_points = np.column_stack((curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z))
-		plot_name = 'lap_location_trail_spline[{}]'.format(int(curr_lap_id))
-		trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
-		size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
-		line = LapsVisualizationMixin.lines_from_points(curr_lap_points)
-		line["scalars"] = np.arange(line.n_points)
-		tube = line.tube(radius=0.2)
-		# tube.plot(smooth_shading=True)
-		interactiveDataExplorer.p.add_mesh(tube, name=plot_name, render_lines_as_tubes=False, show_scalar_bar=False, cmap='fire', lighting=False, render=False)
+    @staticmethod
+    def plot_lap_trajectory_path_spline(p, curr_lap_position_traces, curr_lap_id):
+        num_lap_samples = np.shape(curr_lap_position_traces)[1]
+        lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9 + (0.45 * curr_lap_id))
+        curr_lap_points = np.column_stack((curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z))
+        plot_name = 'lap_location_trail_spline[{}]'.format(int(curr_lap_id))
+        trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
+        size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
+        line = LapsVisualizationMixin.lines_from_points(curr_lap_points)
+        line["scalars"] = np.arange(line.n_points)
+        tube = line.tube(radius=0.2)
+        # tube.plot(smooth_shading=True)
+        p.add_mesh(tube, name=plot_name, render_lines_as_tubes=False, show_scalar_bar=False, cmap='fire', lighting=False, render=False)
