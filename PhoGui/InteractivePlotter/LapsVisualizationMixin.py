abc1c190b50a86803b293ff1cdf3f39ef2f424ea
Pho Hale <PhoHale@gmail.com>
2021-12-07 08:32:12 -0500

    temp

--- PhoGui/InteractivePlotter/LapsVisualizationMixin.py
+++ PhoGui/InteractivePlotter/LapsVisualizationMixin.py
@@ -4,6 +4,8 @@ import pandas as pd
 import pyvista as pv
 
 class LapsVisualizationMixin:
+
+    @staticmethod
     def lines_from_points(points):
 		"""Given an array of points, make a line set"""
 		poly = pv.PolyData()
@@ -14,11 +16,14 @@ class LapsVisualizationMixin:
 		poly.lines = cells
 		return poly
 
-	def get_lap_times(curr_lap_id):
-		return self.session.laps.get_lap_times(curr_lap_id)
 
-	def get_lap_position(self, curr_lap_id):
-		curr_position_df = self.session.position.to_dataframe()
+	@staticmethod
+	def get_lap_times(session, curr_lap_id):
+		return session.laps.get_lap_times(curr_lap_id)
+
+	@staticmethod
+	def get_lap_position(session, curr_lap_id):
+		curr_position_df = session.position.to_dataframe()
 		curr_lap_t_start, curr_lap_t_stop = get_lap_times(curr_lap_id)
 		print('lap[{}]: ({}, {}): '.format(curr_lap_id, curr_lap_t_start, curr_lap_t_stop))
 
@@ -31,36 +36,52 @@ class LapsVisualizationMixin:
 		return curr_lap_position_traces
 
 
-	def plot_lap_trajectory_path(ipspikesDataExplorer, curr_lap_position_traces):
+	@staticmethod
+	def plot_lap_trajectory_path(interactiveDataExplorer, curr_lap_position_traces):
 		num_lap_samples = np.shape(curr_lap_position_traces)[1]
 		lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9)
 		plot_name = 'lap_location_trail'
-		fade_values = ipspikesDataExplorer.params.active_trail_opacity_values
+		fade_values = interactiveDataExplorer.params.active_trail_opacity_values
 		# size_values = ipspikesDataExplorer.params.active_trail_size_values
 		trail_fade_values = None
 		size_values = None
 		trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
 		size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
-		ipspikesDataExplorer.perform_plot_location_trail(plot_name, curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z,
+		interactiveDataExplorer.perform_plot_location_trail(plot_name, curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z,
 													trail_fade_values=trail_fade_values, trail_point_size_values=size_values,
 													render=True, color='red')
 
-	def plot_lap_trajectory_path_spline(ipspikesDataExplorer, curr_lap_position_traces):
+
+	
+	@staticmethod
+	def plot_lap_trajectory_path_spline(interactiveDataExplorer, curr_lap_position_traces):
 		num_lap_samples = np.shape(curr_lap_position_traces)[1]
 		lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9)
 		curr_lap_points = np.column_stack((curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z))
 		plot_name = 'lap_location_trail_spline'
-		fade_values = ipspikesDataExplorer.params.active_trail_opacity_values
+		fade_values = interactiveDataExplorer.params.active_trail_opacity_values
 		# size_values = ipspikesDataExplorer.params.active_trail_size_values
 		trail_fade_values = None
 		size_values = None
 		trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
 		size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
-		line = lines_from_points(curr_lap_points)
+		line = LapsVisualizationMixin.lines_from_points(curr_lap_points)
 		line["scalars"] = np.arange(line.n_points)
 		tube = line.tube(radius=0.5)
 		# tube.plot(smooth_shading=True)
-		ipspikesDataExplorer.p.add_mesh(tube, name=plot_name, render_lines_as_tubes=True, show_scalar_bar=False, color='red')
-		# ipspikesDataExplorer.perform_plot_location_trail(plot_name, curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z,
-		#                                              trail_fade_values=trail_fade_values, trail_point_size_values=size_values,
-		#                                              render=True, color='red')
+		interactiveDataExplorer.p.add_mesh(tube, name=plot_name, render_lines_as_tubes=True, show_scalar_bar=False, color='red')
+  
+  
+	@staticmethod
+	def plot_lap_trajectory_path_spline(interactiveDataExplorer, curr_lap_position_traces, curr_lap_id):
+		num_lap_samples = np.shape(curr_lap_position_traces)[1]
+		lap_fixed_z = np.full_like(curr_lap_position_traces[0,:], 0.9 + (0.45 * curr_lap_id))
+		curr_lap_points = np.column_stack((curr_lap_position_traces[0,:], curr_lap_position_traces[1,:], lap_fixed_z))
+		plot_name = 'lap_location_trail_spline[{}]'.format(int(curr_lap_id))
+		trail_fade_values = np.linspace(0.0, 0.6, num_lap_samples)
+		size_values = np.linspace(0.2, 0.6, num_lap_samples) # fade from a scale of 0.2 to 0.6
+		line = LapsVisualizationMixin.lines_from_points(curr_lap_points)
+		line["scalars"] = np.arange(line.n_points)
+		tube = line.tube(radius=0.2)
+		# tube.plot(smooth_shading=True)
+		interactiveDataExplorer.p.add_mesh(tube, name=plot_name, render_lines_as_tubes=False, show_scalar_bar=False, cmap='fire', lighting=False, render=False)
