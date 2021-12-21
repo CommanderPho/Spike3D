5a5b0a300c1e431c26c8c96e09cec1524694e5dd
Pho Hale <PhoHale@gmail.com>
2021-12-21 17:50:28 -0500

    using new PfNd class

--- PendingNotebookCode.py
+++ PendingNotebookCode.py
@@ -1,5 +1,5 @@
 ## This file serves as overflow from active Jupyter-lab notebooks, to eventually be refactored.
-
+from copy import deepcopy
 from matplotlib.colors import ListedColormap
 from pathlib import Path
 import numpy as np
@@ -10,9 +10,17 @@ import pyvistaqt as pvqt # conda install -c conda-forge pyvistaqt
 from PhoPositionalData.analysis.interactive_placeCell_config import InteractivePlaceCellConfig, VideoOutputModeConfig, PlottingConfig  # VideoOutputModeConfig, InteractivePlaceCellConfigs
 from PhoPositionalData.analysis.interactive_placeCell_config import print_subsession_neuron_differences
 
-from neuropy.analyses import perform_compute_placefields, plot_all_placefields
+
+from neuropy.core import Laps
+
+from neuropy.analyses import perform_compute_placefields
 from neuropy.analyses.placefields import PlacefieldComputationParameters
+from neuropy.analyses.laps import estimate_laps, compute_laps_spike_indicies
+
 from neuropy.plotting.spikes import get_neuron_colors
+from neuropy.plotting.placemaps import plot_all_placefields
+
+from PhoPositionalData.plotting.laps import plot_laps_2d
 
 should_force_recompute_placefields = True
 should_display_2D_plots = True
@@ -33,7 +41,8 @@ def debug_print_spike_counts(session):
 def compute_placefields_as_needed(active_session, computation_config=None, general_config: InteractivePlaceCellConfig=None, active_placefields1D = None, active_placefields2D = None, included_epochs=None, should_force_recompute_placefields=False, should_display_2D_plots=False):
     if computation_config is None:
         computation_config = PlacefieldComputationParameters(speed_thresh=9, grid_bin=2, smooth=0.5)
-    active_placefields1D, active_placefields2D = perform_compute_placefields(active_session.neurons, active_session.position, computation_config, active_placefields1D, active_placefields2D, included_epochs=included_epochs, should_force_recompute_placefields=True)
+    # active_placefields1D, active_placefields2D = perform_compute_placefields(active_session.neurons, active_session.position, computation_config, active_placefields1D, active_placefields2D, included_epochs=included_epochs, should_force_recompute_placefields=True)
+    active_placefields1D, active_placefields2D = perform_compute_placefields(active_session.spikes_df, active_session.position, computation_config, active_placefields1D, active_placefields2D, included_epochs=included_epochs, should_force_recompute_placefields=True)
     # Plot the placefields computed and save them out to files:
     if should_display_2D_plots:
         ax_pf_1D, occupancy_fig, active_pf_2D_figures = plot_all_placefields(active_placefields1D, active_placefields2D, general_config)
@@ -106,4 +115,38 @@ def build_placefield_multiplotter(nfields, linear_plot_data=None):
             mp[curr_row, curr_col].add_mesh(linear_plot_data[a_linear_index], name='maze_bg', color="black", render=False)
             # mp[a_row_column_index[0], a_row_column_index[1]].add_mesh(pv.Sphere())
     return mp, linear_plotter_indicies, row_column_indicies
+
+def partition(df: pd.DataFrame, partitionColumn: str):
+    # splits a DataFrame df on the unique values of a specified column (partitionColumn) to return a unique DataFrame for each unique value in the column.
+    unique_values = np.unique(df[partitionColumn]) # array([ 0,  1,  2,  3,  4,  7, 11, 12, 13, 14])
+    grouped_df = df.groupby([partitionColumn]) #  Groups on the specified column.
+    return unique_values, np.array([grouped_df.get_group(aValue) for aValue in unique_values], dtype=object) # dataframes split for each unique value in the column
+
+
+def estimation_session_laps(sess, N=20):
+    """ 2021-12-21 - Pho's lap estimation from the position data (only)
+    Replaces the sess.laps which is computed or loaded from the spikesII.mat spikes data (which isn't very good)"""
+    # backup the extant laps object to prepare for the new one:
+    sess.old_laps_obj = deepcopy(sess.laps)
+    # plot originals:
+    fig, out_axes_list = plot_laps_2d(sess, legacy_plotting_mode=True)
+    out_axes_list[0].set_title('Old SpikeII computed Laps')
+    position_obj = sess.position
+    # position_obj.dt
+    position_obj.compute_higher_order_derivatives()
+    pos_df = position_obj.compute_smoothed_position_info(N=N) ## Smooth the velocity curve to apply meaningful logic to it
+    pos_df = position_obj.to_dataframe()
+    # custom_test_laps = deepcopy(sess.laps)
+    spikes_df = deepcopy(sess.spikes_df)
+
+    desc_crossing_beginings, desc_crossing_midpoints, desc_crossing_endings, asc_crossing_beginings, asc_crossing_midpoints, asc_crossing_endings = estimate_laps(pos_df)
+    custom_test_laps_obj = Laps.from_estimated_laps(pos_df['t'].to_numpy(), desc_crossing_beginings, desc_crossing_endings, asc_crossing_beginings, asc_crossing_endings)
+    ## Determine the spikes included with each computed lap:
+    custom_test_laps_obj = compute_laps_spike_indicies(custom_test_laps_obj, spikes_df)
+    sess.laps = deepcopy(custom_test_laps_obj) # replace the laps obj
+    # plot computed:
+    fig, out_axes_list = plot_laps_2d(sess, legacy_plotting_mode=False)
+    out_axes_list[0].set_title('New Pho Position Thresholding Estimated Laps')
+
+    return sess
 
