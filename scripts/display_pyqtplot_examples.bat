5465d096e48503f762700f7d4dee7ffdbf916f83
Pho Hale <CommanderPho@users.noreply.github.com>
2022-02-19 14:31:28 -0500

    added Windows .bat script that launches the pyqtplot examples

---
+++ scripts/display_pyqtplot_examples.bat
@@ -0,0 +1,10 @@
+@echo off
+echo display_pyqtplot_examples.bat
+@REM call C:/Users/pho/miniconda3/Scripts/activate
+@REM conda activate phoviz_ultimate
+@REM START /B "C:/Users/pho/miniconda3/envs/phoviz_ultimate/python.exe" "c:/Users/pho/repos/PhoPy3DPositionAnalysis2021/PyQtPlotExamples/PyQtPlot_EXAMPLES.py"
+"C:/Users/pho/miniconda3/envs/phoviz_ultimate/python.exe" "c:/Users/pho/repos/PhoPy3DPositionAnalysis2021/PyQtPlotExamples/PyQtPlot_EXAMPLES.py"
+
+echo "done"
+pause
+
