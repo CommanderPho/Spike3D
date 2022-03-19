8202ce98d7c95a133f2946c5b0d5cae9efd03c34
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-18 20:40:58 -0400

    made example work

--- PyQtPlotExamples/NonWorking/pyqtgraph_PyqtGraphScenery.py
+++ PyQtPlotExamples/NonWorking/pyqtgraph_PyqtGraphScenery.py
@@ -208,5 +208,12 @@ VC=(('LOL', 3,1,(4.5,2,7),(110,0,0),(0.5,1,0.5,0.2)),
 #Create the objects:
 
 Buildings(VE)
-Captadores(VE)
+Captadores(VC)
 
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
+        
+        
