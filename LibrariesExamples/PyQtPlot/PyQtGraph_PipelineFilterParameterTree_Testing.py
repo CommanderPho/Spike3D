c150127ae3aceb6017f8ccc693ca8587523c6f23
Pho Hale <CommanderPho@users.noreply.github.com>
2022-07-19 17:28:45 -0400

    135, minor stylesheet changes

--- LibrariesExamples/PyQtPlot/PyQtGraph_PipelineFilterParameterTree_Testing.py
+++ LibrariesExamples/PyQtPlot/PyQtGraph_PipelineFilterParameterTree_Testing.py
@@ -27,7 +27,6 @@ from pyphoplacecellanalysis.GUI.PyQtPlot.Params.SaveRestoreStateParamHelpers imp
 
 ## For qdarkstyle theming support:
 import qdarkstyle
-# app.setStyleSheet(qdarkstyle.load_stylesheet_from_environment(is_pyqtgraph=True))
 # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 
 # For BreezeStylesheets support:
@@ -44,7 +43,7 @@ import pyphoplacecellanalysis.External.breeze_style_sheets.breeze_resources
 
 app = pg.mkQApp("Parameter Tree Filter Options")
 # app.setStyleSheet(stylesheet_data_stream.readAll())
-app.setStyleSheet(qdarkstyle.load_stylesheet_from_environment(is_pyqtgraph=True)) # QDarkStyle version
+app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5()) # QDarkStyle version
 
 
 
