4a7453f1908e6e0d0f8931efb6bdf2dc633c0eb4
Pho Hale <CommanderPho@users.noreply.github.com>
2022-09-06 04:14:08 -0400

    bump

--- LibrariesExamples/PyQt/testCustomContextMenu.py
+++ LibrariesExamples/PyQt/testCustomContextMenu.py
@@ -1,9 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMenu, QListWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QMenu, QListWidget, QVBoxLayout, QAction
 from PyQt5.QtCore import QEvent, Qt
+# from PyQt5.QtGui import QActionEvent
 
 from pyphoplacecellanalysis.Resources import GuiResources, ActionIcons
+from pyphoplacecellanalysis.GUI.Qt.Mixins.PhoMenuHelper import PhoMenuHelper
 from pyphoplacecellanalysis.GUI.Qt.GlobalApplicationMenus.LocalMenus_AddRenderable import LocalMenus_AddRenderable
+from pyphoplacecellanalysis.GUI.Qt.Mixins.Menus.BaseMenuProviderMixin import BaseMenuProviderMixin
 
 
 class MyApp(QWidget):
@@ -40,6 +43,68 @@ class MyApp(QWidget):
         return super().eventFilter(source, event)
 
 
+def create_custom_context_menu(owner):
+    # # create context menu
+    # self.popMenu = QtGui.QMenu(self)
+    # self.popMenu.addAction(QtGui.QAction('test0', self))
+    # self.popMenu.addAction(QtGui.QAction('test1', self))
+    # self.popMenu.addSeparator()
+    # self.popMenu.addAction(QtGui.QAction('test2', self))
+
+    popMenuActionsList = [QAction('test0', parent=owner),
+        QAction('test1', parent=owner),
+        QAction('test2', parent=owner)]
+    
+    
+    ## create context menu
+    owner.popMenu = QMenu(parent=owner)
+    # ## Inline Actions:
+    # owner.popMenu.addAction(QAction('test0', parent=owner))
+    # owner.popMenu.addAction(QAction('test1', parent=owner))
+    # owner.popMenu.addSeparator()
+    # owner.popMenu.addAction(QAction('test2', parent=owner))
+        
+    # owner.popMenu.addAction(QAction('test0', parent=owner))
+    # owner.popMenu.addAction(QAction('test1', parent=owner))
+    # owner.popMenu.addSeparator()
+    # owner.popMenu.addAction(QAction('test2', parent=owner))
+    
+    nested_action_parent = popMenuActionsList[1]
+    # nested_action_parent = owner
+    # nested_action_parent = owner.popMenu
+    nestedMenuActionsList = [QAction('testA', parent=nested_action_parent),
+        QAction('testB', parent=nested_action_parent),
+        QAction('testC', parent=nested_action_parent)]
+    
+        
+    # nested_menu = owner.popMenu
+    # # nested_menu = owner.subMenu
+    # nested_menu.addAction(QAction('testA', parent=owner.popMenu))
+    # nested_menu.addAction(QAction('testB', parent=owner.popMenu))
+    # nested_menu.addSeparator()
+    # nested_menu.addAction(QAction('testC', parent=owner.popMenu))
+    # # Add a submenu
+    # # owner.subMenu = QMenu(owner.popMenu)
+    # # owner.subMenu = QMenu(owner)
+    
+    for an_action in popMenuActionsList:
+        owner.popMenu.addAction(an_action)
+        
+    for an_action in nestedMenuActionsList:
+        owner.popMenu.addAction(an_action)
+    
+    # owner.subMenu = QMenu(owner.popMenu)
+    # owner.subMenu = QMenu(owner)
+    # owner.subMenu.addAction(QAction('testA', owner.popMenu))
+    # owner.subMenu.addAction(QAction('testB', owner.popMenu))
+    # owner.subMenu.addSeparator()
+    # owner.subMenu.addAction(QAction('testC', owner.popMenu))
+    # subMenuRemoveAction = owner.popMenu.addMenu(owner.subMenu)
+
+    return owner.popMenu
+ 
+ 
+ 
 # testCustomContextMenu
 
 if __name__ == '__main__':
@@ -47,17 +112,21 @@ if __name__ == '__main__':
     QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     
     app = QApplication(sys.argv)
-    app.setStyleSheet('''
-        QWidget {
-            font-size: 30px;
-        }
-    ''')
+    # app.setStyleSheet('''
+    #     QWidget {
+    #         font-size: 30px;
+    #     }
+    # ''')
     
-    widget = LocalMenus_AddRenderable()
-    provided_menu = widget.ui.menuAdd_Renderable
+    # widget = LocalMenus_AddRenderable()
+    # provided_menu = widget.ui.menuAdd_Renderable
+    # myApp = MyApp(custom_menu=provided_menu)
     # widget.show()
     
-    myApp = MyApp(custom_menu=provided_menu)
+    myApp = MyApp(custom_menu=None)
+    provided_menu = create_custom_context_menu(myApp)
+    print(f'provided_menu: {provided_menu}')
+    myApp.custom_menu = provided_menu
     myApp.show()
 
     try:
