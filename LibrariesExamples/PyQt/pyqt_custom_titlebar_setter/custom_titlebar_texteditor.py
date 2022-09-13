259386e36a907f7829f83833dfd78cb49ebd3bbf
Pho Hale <CommanderPho@users.noreply.github.com>
2022-09-13 08:45:20 -0400

    196

--- LibrariesExamples/PyQt/pyqt_custom_titlebar_setter/custom_titlebar_texteditor.py
+++ LibrariesExamples/PyQt/pyqt_custom_titlebar_setter/custom_titlebar_texteditor.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QTextEdit
-from pyqt_custom_titlebar_setter import CustomTitlebarSetter
+# from pyqt_custom_titlebar_setter import CustomTitlebarSetter
+from pyqt_custom_titlebar_window import CustomTitlebarWindow
 
 
 class TextEditor(QWidget):
@@ -19,6 +20,13 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = TextEditor()
     # window = CustomTitlebarSetter.getCustomTitleBarWindow(main_window=widget, icon_filename='dark-notepad.svg')
-    window = CustomTitlebarSetter.getCustomTitleBarWindow(main_window=widget, icon_filename='', title='TEST')
+    # window = CustomTitlebarSetter.getCustomTitleBarWindow(main_window=widget, title='TEST')
+    
+    window = CustomTitlebarWindow(widget)
+    # window.setTopTitleBar(title='test', bottom_separator=True)
+    # window.setTopTitleBar(title='test', icon_filename='', font=font, align=align, bottom_separator=bottom_separator)
+    # window.setButtonHint(hint)
+    # window.setButtons()
+    
     window.show()
     sys.exit(app.exec_())
