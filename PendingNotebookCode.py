497f584478f4bc9c275d10b909f929b590c89085
Pho Hale <CommanderPho@users.noreply.github.com>
2022-12-15 20:36:17 -0500

    working

--- PendingNotebookCode.py
+++ PendingNotebookCode.py
@@ -18,6 +18,67 @@ from neuropy.core.epoch import NamedTimerange
 should_force_recompute_placefields = True
 should_display_2D_plots = True
 
+# ==================================================================================================================== #
+# 2022-12-15 Importing from TestNeuropyPipeline241                                                                     #
+# ==================================================================================================================== #
+
+
+# ### ✅ Test adding the nearest predicted/decoded position as a red point to the 3D plotter:
+
+# In[ ]:
+
+
+## Sync ipspikesDataExplorer to raster window:
+extra_interactive_spike_behavior_browser_sync_connection = spike_raster_window.connect_additional_controlled_plotter(controlled_plt=ipspikesDataExplorer)
+# extra_interactive_spike_behavior_browser_sync_connection = _connect_additional_controlled_plotter(spike_raster_window.spike_raster_plt_2d, ipspikesDataExplorer)
+
+_debug_print = False
+
+def _update_nearest_decoded_most_likely_position_callback(start_t, end_t):
+    """ Only uses end_t
+    Implicitly captures: ipspikesDataExplorer, _get_nearest_decoded_most_likely_position_callback
+    
+    Usage:
+        _update_nearest_decoded_most_likely_position_callback(0.0, ipspikesDataExplorer.t[0])
+        _conn = ipspikesDataExplorer.sigOnUpdateMeshes.connect(_update_nearest_decoded_most_likely_position_callback)
+
+    """
+    def _get_nearest_decoded_most_likely_position_callback(t):
+        """ A callback that when passed a visualization timestamp (the current time to render) returns the most likely predicted position provided by the active_two_step_decoder
+        Implicitly captures:
+            active_one_step_decoder, active_two_step_decoder
+        Usage:
+            _get_nearest_decoded_most_likely_position_callback(9000.1)
+        """
+        active_time_window_variable = active_one_step_decoder.time_window_centers # get time window centers (n_time_window_centers,) # (4060,)
+        active_most_likely_positions = active_one_step_decoder.most_likely_positions.T # (4060, 2) NOTE: the most_likely_positions for the active_one_step_decoder are tranposed compared to the active_two_step_decoder
+        # active_most_likely_positions = active_two_step_decoder.most_likely_positions # (2, 4060)
+        assert np.shape(active_time_window_variable)[0] == np.shape(active_most_likely_positions)[1], f"timestamps and num positions must be the same but np.shape(active_time_window_variable): {np.shape(active_time_window_variable)} and np.shape(active_most_likely_positions): {np.shape(active_most_likely_positions)}!"
+        last_window_index = np.searchsorted(active_time_window_variable, t, side='left') # side='left' ensures that no future values (later than 't') are ever returned
+        # TODO: CORRECTNESS: why is it returning an index that corresponds to a time later than the current time?
+        # for current time t=9000.0
+        #     last_window_index: 1577
+        #     last_window_time: 9000.5023
+        # EH: close enough
+        last_window_time = active_time_window_variable[last_window_index] # If there is no suitable index, return either 0 or N (where N is the length of `a`).
+        displayed_time_offset = t - last_window_time # negative value if the window time being displayed is in the future
+        if _debug_print:
+            print(f'for current time t={t}\n\tlast_window_index: {last_window_index}\n\tlast_window_time: {last_window_time}\n\tdisplayed_time_offset: {displayed_time_offset}')
+        return (last_window_time, *list(np.squeeze(active_most_likely_positions[:, last_window_index]).copy()))
+
+    t = end_t # the t under consideration should always be the end_t. This is written this way just for compatibility with the ipspikesDataExplorer.sigOnUpdateMeshes (float, float) signature
+    curr_t, curr_x, curr_y = _get_nearest_decoded_most_likely_position_callback(t)
+    curr_debug_point = [curr_x, curr_y, ipspikesDataExplorer.z_fixed[-1]]
+    if _debug_print:
+        print(f'tcurr_debug_point: {curr_debug_point}') # \n\tlast_window_time: {last_window_time}\n\tdisplayed_time_offset: {displayed_time_offset}
+    ipspikesDataExplorer.perform_plot_location_point('debug_point_plot', curr_debug_point, color='r', render=True)
+    return curr_debug_point
+
+_update_nearest_decoded_most_likely_position_callback(0.0, ipspikesDataExplorer.t[0])
+# _conn = pg.SignalProxy(ipspikesDataExplorer.sigOnUpdateMeshes, rateLimit=14, slot=_update_nearest_decoded_most_likely_position_callback)
+_conn = ipspikesDataExplorer.sigOnUpdateMeshes.connect(_update_nearest_decoded_most_likely_position_callback)
+
+
 
 
 # ==================================================================================================================== #
