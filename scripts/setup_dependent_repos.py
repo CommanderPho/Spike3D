a779598c667d8d59c6a1125a9624aca7e6697f68
CommanderPho <PhoHale@gmail.com>
2023-03-01 14:51:45 -0500

    added _reset_local_changes

--- scripts/setup_dependent_repos.py
+++ scripts/setup_dependent_repos.py
@@ -40,17 +40,29 @@ external_dependent_binary_repo_urls = ["https://github.com/CommanderPho/pyqode.c
 external_dependent_repo_paths = [root_dir.joinpath(a_rel_path).resolve() for a_rel_path in external_dependent_repos]
 binary_repo_tuples = list(zip(external_dependent_repo_paths, external_dependent_binary_repo_urls, [True]*len(external_dependent_repo_paths)))
 
+def _reset_local_changes(repo_path):
+    """ Resets local changes to the repo."""
+    os.chdir(repo_path)
+    # os.system("git reset --hard HEAD")
+    # os.system("git clean -f -d")
+    os.system("git stash; git stash drop")
 
 
 def setup_repo(repo_path, repo_url, is_binary_repo=False):
+    """ Clones the repo if it doesn't exist, and updates it if it does."""
     if not repo_path.exists():
         # clone the repo
         os.chdir(repo_path.parent) # change directory to the parent of the repo to prepare for cloning
         print(f'\t repo does not exist. Cloning {repo_path}...')
         os.system(f'git clone {repo_url}')
-
-    os.chdir(repo_path)
-    os.system("git pull")
+        os.chdir(repo_path)
+    else:
+        # update existing repo
+        print(f'\t repo exists. Updating {repo_path}...')
+        os.chdir(repo_path)
+        _reset_local_changes(repo_path)
+        # new files that are local only still hold things up
+        os.system("git pull")
 
     if is_binary_repo:
         ## For binary repos:
