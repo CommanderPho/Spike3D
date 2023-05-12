a8992cbacb0480723d655657f064ecbef30d6877
Pho Hale <PhoHale@gmail.com>
2023-05-12 04:36:45 -0400

    Code Indexing

---
+++ SCRATCH/code_symbols_extraction_with_CodeQuery.cmd
@@ -0,0 +1,85 @@
+ctags -R --languages=python .
+
+C:/Users/pho/repos/Spike3DWorkEnv/NeuroPy/neuropy
+C:/Users/pho/repos/Spike3DWorkEnv/pyPhoCoreHelpers/src/pyphocorehelpers
+C:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis
+C:/Users/pho/repos/Spike3DWorkEnv/Spike3D
+
+ctags -R --languages=python C:/Users/pho/repos/Spike3DWorkEnv/NeuroPy/neuropy C:/Users/pho/repos/Spike3DWorkEnv/pyPhoCoreHelpers/src/pyphocorehelpers C:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis C:/Users/pho/repos/Spike3DWorkEnv/Spike3D
+
+
+
+
+ctags -R --languages=python C:\Users\pho\repos\Spike3DWorkEnv\NeuroPy\neuropy C:\Users\pho\repos\Spike3DWorkEnv\pyPhoCoreHelpers\src\pyphocorehelpers C:\Users\pho\repos\Spike3DWorkEnv\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis C:\Users\pho\repos\Spike3DWorkEnv\Spike3D -f SCRIPTS\.tags
+
+
+pycscope -R C:\Users\pho\repos\Spike3DWorkEnv\NeuroPy\neuropy C:\Users\pho\repos\Spike3DWorkEnv\pyPhoCoreHelpers\src\pyphocorehelpers C:\Users\pho\repos\Spike3DWorkEnv\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis C:\Users\pho\repos\Spike3DWorkEnv\Spike3D -f SCRIPTS\pycscope.out
+
+
+
+
+Search 
+
+`In print(f'') strings`
+In strings (between "")
+In comments
+
+
+
+python build symbol tree GUI utility
+
+cd "C:\Users\pho\repos\Spike3DWorkEnv\NeuroPy\neuropy"
+dir /b/a/s *.py    > cscope.files
+pycscope -i cscope.files
+ctags --fields=+i -n -L cscope.files
+cqmakedb -s .\myproject.db -c cscope.out -t tags -p
+
+
+cd "C:\Users\pho\repos\Spike3DWorkEnv\pyPhoCoreHelpers\src"
+dir /b/a/s *.py    > cscope.files
+pycscope -i cscope.files
+ctags --fields=+i -n -L cscope.files
+cqmakedb -s .\myproject.db -c cscope.out -t tags -p
+
+
+cd "C:\Users\pho\repos\Spike3DWorkEnv\pyPhoPlaceCellAnalysis\src"
+dir /b/a/s *.py | find /i /v "External\\" | findstr /i /v "\\External\\"   > cscope.files 
+
+
+# NOTE: Works when excluding all External/* files
+pycscope -i cscope.files
+ctags --fields=+i -n -L cscope.files
+cqmakedb -s .\myproject.db -c cscope.out -t tags -p
+
+
+# This does not work: the idea of adding them all to a shared database
+cqmakedb -s ..\..\full_project.db -c cscope.out -t tags -p
+
+
+```python
+
+import subprocess
+import pathlib
+
+def build_code_index(project_path):
+    project_path = pathlib.Path(project_path)
+    cscope_files = project_path / "cscope.files"
+    cscope_out = project_path / "cscope.out"
+    tags = project_path / "tags"
+    db_path = project_path / "myproject.db"
+    
+    # Generate the cscope index
+    subprocess.run(["pycscope", "-i", str(cscope_files)], cwd=str(project_path), check=True)
+    
+    # Generate the ctags index
+    subprocess.run(["ctags", "--fields=+i", "-n", "-L", str(cscope_files)], cwd=str(project_path), check=True)
+    
+    # Generate the CodeQL database
+    subprocess.run(["cqmakedb", "-s", str(db_path), "-c", str(cscope_out), "-t", str(tags), "-p"], cwd=str(project_path), check=True)
+
+
+
+
+build_code_index("C:/Users/pho/repos/Spike3DWorkEnv/pyPhoCoreHelpers/src")
+
+```
---
+++ scripts/generate_code_index.py
@@ -0,0 +1,83 @@
+import subprocess
+import pathlib
+
+
+
+def build_code_index(project_path, exclude_dirs=[]):
+    """ 2023-05-11 - Tries to build a CodeQuery code index. Not quite working.
+    
+    Original Bash Commands:
+    
+
+        cd "C:/Users/pho/repos/Spike3DWorkEnv/NeuroPy/neuropy"
+        dir /b/a/s *.py    > cscope.files
+        pycscope -i cscope.files
+        ctags --fields=+i -n -L cscope.files
+        cqmakedb -s .\myproject.db -c cscope.out -t tags -p
+
+
+        cd "C:/Users/pho/repos/Spike3DWorkEnv/pyPhoCoreHelpers/src"
+        dir /b/a/s *.py    > cscope.files
+        pycscope -i cscope.files
+        ctags --fields=+i -n -L cscope.files
+        cqmakedb -s .\myproject.db -c cscope.out -t tags -p
+
+
+        cd "C:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src"
+        dir /b/a/s *.py | find /i /v "External\\" | findstr /i /v "\\External\\"   > cscope.files 
+
+        # NOTE: Works when excluding all External/* files
+        pycscope -i cscope.files
+        ctags --fields=+i -n -L cscope.files
+        cqmakedb -s .\myproject.db -c cscope.out -t tags -p
+
+
+        # This does not work: the idea of adding them all to a shared database
+        cqmakedb -s ..\..\full_project.db -c cscope.out -t tags -p
+
+    
+    Example:
+        project_path = "C:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src"
+        exclude_dirs = ["pyphoplacecellanalysis/External"]
+
+    """
+    project_path = pathlib.Path(project_path)
+    cscope_files = project_path / "cscope.files"
+    cscope_out = project_path / "cscope.out"
+    tags = project_path / "tags"
+    db_path = project_path / "myproject.db"
+
+    # Find all .py files in the project directory and its subdirectories
+    py_files = project_path.glob("**/*.py")
+    py_files = [file_path for file_path in py_files] # to list
+
+    excluded_py_files = []
+    if exclude_dirs is not None:
+        # Find all .py files in the project directory and its subdirectories, excluding the 'my_exclude_dir' directory
+        exclude_paths = [project_path.joinpath(a_dir) for a_dir in exclude_dirs]
+        for an_exclude_path in exclude_paths:
+            excluded_py_files.extend([file_path for file_path in an_exclude_path.glob("**/*.py")])
+
+    included_py_files = [x for x in py_files if x not in excluded_py_files]
+
+    # Write the file paths to cscope.files
+    with open(project_path / "cscope.files", "w") as f:
+        for file_path in included_py_files:
+            f.write(str(file_path) + "\n")
+
+    # Generate the cscope index
+    subprocess.run(["pycscope", "-i", str(cscope_files)], cwd=str(project_path), check=True)
+    
+    # Generate the ctags index
+    subprocess.run(["ctags", "--fields=+i", "-n", "-L", str(cscope_files)], cwd=str(project_path), check=True)
+    
+    # Generate the CodeQL database
+    subprocess.run(["cqmakedb", "-s", str(db_path), "-c", str(cscope_out), "-t", str(tags), "-p"], cwd=str(project_path), check=True)
+
+
+
+build_code_index("C:/Users/pho/repos/Spike3DWorkEnv/NeuroPy/neuropy")
+build_code_index("C:/Users/pho/repos/Spike3DWorkEnv/pyPhoCoreHelpers/src")
+build_code_index("C:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src", exclude_dirs = ["pyphoplacecellanalysis/External"])
+
+
