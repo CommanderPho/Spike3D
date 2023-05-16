674737c23a7763fd64f911e14a8756750f8f3cef
Pho Hale <PhoHale@gmail.com>
2023-05-16 15:03:25 -0400

    matplotlib examples

---
+++ LibrariesExamples/Matplotlib/span_selector.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import SpanSelector
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+fig, (ax1, ax2) = plt.subplots(2, figsize=(8, 6))
+
+x = np.arange(0.0, 5.0, 0.01)
+y = np.sin(2 * np.pi * x) + 0.5 * np.random.randn(len(x))
+
+ax1.plot(x, y)
+ax1.set_ylim(-2, 2)
+ax1.set_title('Press left mouse button and drag '
+              'to select a region in the top graph')
+
+line2, = ax2.plot([], [])
+
+
+def onselect(xmin, xmax):
+    indmin, indmax = np.searchsorted(x, (xmin, xmax))
+    indmax = min(len(x) - 1, indmax)
+
+    region_x = x[indmin:indmax]
+    region_y = y[indmin:indmax]
+
+    if len(region_x) >= 2:
+        line2.set_data(region_x, region_y)
+        ax2.set_xlim(region_x[0], region_x[-1])
+        ax2.set_ylim(region_y.min(), region_y.max())
+        fig.canvas.draw_idle()
+
+
+span = SpanSelector(
+    ax1,
+    onselect,
+    "horizontal",
+    useblit=True,
+    props=dict(alpha=0.5, facecolor="tab:blue"),
+    interactive=True,
+    drag_from_anywhere=True
+)
+# Set useblit=True on most backends for enhanced performance.
+
+
+plt.show()
