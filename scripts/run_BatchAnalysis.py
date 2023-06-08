a18b6bfa3bb52be1536ae6f96e53ca8ecdb6aea4
Pho Hale <PhoHale@gmail.com>
2023-06-08 17:45:29 -0400

    renamed NonInteractiveWrapper to NonInteractiveProcessing

--- scripts/run_BatchAnalysis.py
+++ scripts/run_BatchAnalysis.py
@@ -27,16 +27,16 @@ from neuropy.utils.result_context import IdentifyingContext
 from neuropy.core.session.Formats.BaseDataSessionFormats import find_local_session_paths
 
 # from PendingNotebookCode import _perform_batch_plot, _build_batch_plot_kwargs
-from pyphoplacecellanalysis.General.Batch.NonInteractiveWrapper import batch_load_session, batch_extended_computations, SessionBatchProgress, batch_programmatic_figures, batch_extended_programmatic_figures
+from pyphoplacecellanalysis.General.Batch.NonInteractiveProcessing import batch_load_session, batch_extended_computations, SessionBatchProgress, batch_programmatic_figures, batch_extended_programmatic_figures
 from pyphoplacecellanalysis.General.Pipeline.NeuropyPipeline import PipelineSavingScheme
 from pyphoplacecellanalysis.General.Pipeline.Stages.Loading import saveData, loadData
 from pyphoplacecellanalysis.General.Batch.runBatch import BatchRun
 from pyphoplacecellanalysis.General.Batch.runBatch import run_diba_batch
 from pyphoplacecellanalysis.General.Pipeline.Stages.ComputationFunctions.MultiContextComputationFunctions.LongShortTrackComputations import LongShortPipelineTests
-from pyphoplacecellanalysis.General.Batch.NonInteractiveWrapper import neptune_output_figures
+from pyphoplacecellanalysis.General.Batch.NonInteractiveProcessing import neptune_output_figures
 
 ## Post Compute Validate 2023-05-16:
-from pyphoplacecellanalysis.General.Batch.NonInteractiveWrapper import _update_pipeline_missing_preprocessing_parameters
+from pyphoplacecellanalysis.General.Batch.NonInteractiveProcessing import _update_pipeline_missing_preprocessing_parameters
 
 def post_compute_validate(curr_active_pipeline):
     """ 2023-05-16 - Ensures that the laps are used for the placefield computation epochs, the number of bins are the same between the long and short tracks. """
@@ -56,7 +56,7 @@ def post_compute_validate(curr_active_pipeline):
 
 def _perform_plots(curr_active_pipeline, enable_neptune=True):
     """ 2023-05-25 - Performs all the batch plotting commands. """
-    from pyphoplacecellanalysis.General.Batch.NonInteractiveWrapper import neptune_output_figures
+    from pyphoplacecellanalysis.General.Batch.NonInteractiveProcessing import neptune_output_figures
     
     curr_active_pipeline.reload_default_display_functions()
     try:
