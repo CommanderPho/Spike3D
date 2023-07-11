1ea1087eb67d5b3f1e41a3dd471583ee3cf791db
Pho Hale <PhoHale@gmail.com>
2023-07-11 16:09:23 -0400

    Moved PhoDiba2023Paper out to `pyphoplacecellanalysis.General.Batch.PhoDiba2023Paper`

--- scripts/run_BatchAnalysis.py
+++ scripts/run_BatchAnalysis.py
@@ -37,9 +37,7 @@ from pyphoplacecellanalysis.General.Pipeline.Stages.ComputationFunctions.MultiCo
 # from pyphoplacecellanalysis.General.Batch.NeptuneAiHelpers import set_environment_variables, neptune_output_figures
 from pyphoplacecellanalysis.General.Batch.NonInteractiveProcessing import batch_perform_all_plots, _update_pipeline_missing_preprocessing_parameters
 
-from PhoDiba2023Paper import main_complete_figure_generations
-
-
+from pyphoplacecellanalysis.General.Batch.PhoDiba2023Paper import main_complete_figure_generations
 
 
 
