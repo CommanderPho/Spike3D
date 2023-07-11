869e7dcb31970289411e887e616a383f25965281
Pho Hale <PhoHale@gmail.com>
2023-07-11 06:31:19 -0400

    added example for getting current selected epochs

--- PhoDiba2023Paper.py
+++ PhoDiba2023Paper.py
@@ -124,11 +124,45 @@ def build_shared_sorted_neuronIDs(ratemap, included_unit_neuron_IDs, sort_ind):
 
 @define(slots=False)
 class UserAnnotationsManager:
-    """ class for holding User Annotations of the data. Performed interactive by the user, and then saved to disk for later use. An example are the selected replays to be used as examples. """
+    """ class for holding User Annotations of the data. Performed interactive by the user, and then saved to disk for later use. An example are the selected replays to be used as examples. 
+    
+    Usage:
+        from PhoDiba2023Paper import UserAnnotationsManager
+        
+    """
     @staticmethod
     def get_user_annotations():
         """ hardcoded user annotations
         
+
+        New Entries can be generated like:
+            from pyphoplacecellanalysis.GUI.Qt.Mixins.PaginationMixins import SelectionsObject
+            from PhoDiba2023Paper import UserAnnotationsManager
+            from pyphoplacecellanalysis.Pho2D.stacked_epoch_slices import DecodedEpochSlicesPaginatedFigureController
+
+            ## Stacked Epoch Plot
+            example_stacked_epoch_graphics = curr_active_pipeline.display('_display_long_and_short_stacked_epoch_slices', defer_render=False, save_figure=True)
+            pagination_controller_L, pagination_controller_S = example_stacked_epoch_graphics.plot_data['controllers']
+            ax_L, ax_S = example_stacked_epoch_graphics.axes
+            final_figure_context_L, final_context_S = example_stacked_epoch_graphics.context
+
+            user_annotations = UserAnnotationsManager.get_user_annotations()
+
+            ## Capture current user selection
+            saved_selection_L: SelectionsObject = pagination_controller_L.save_selection()
+            saved_selection_S: SelectionsObject = pagination_controller_S.save_selection()
+            final_L_context = saved_selection_L.figure_ctx.adding_context_if_missing(user_annotation='selections')
+            final_S_context = saved_selection_S.figure_ctx.adding_context_if_missing(user_annotation='selections')
+            user_annotations[final_L_context] = saved_selection_L.flat_all_data_indicies[saved_selection_L.is_selected]
+            user_annotations[final_S_context] = saved_selection_S.flat_all_data_indicies[saved_selection_S.is_selected]
+            # Updates the context. Needs to generate the code.
+
+            ## Generate code to insert int user_annotations:
+            print('Add the following code to UserAnnotationsManager.get_user_annotations() function body:')
+            print(f"user_annotations[{final_L_context.get_initialization_code_string()}] = np.array({list(saved_selection_L.flat_all_data_indicies[saved_selection_L.is_selected])})")
+            print(f"user_annotations[{final_S_context.get_initialization_code_string()}] = np.array({list(saved_selection_S.flat_all_data_indicies[saved_selection_S.is_selected])})")
+
+
         Usage:
             user_anootations = get_user_annotations()
             user_anootations
@@ -143,6 +177,11 @@ class UserAnnotationsManager:
         ## IdentifyingContext(format_name='kdiba',animal='gor01',exper_name='two',session_name='2006-6-07_16-40-19')
         user_annotations[IdentifyingContext(format_name='kdiba',animal='gor01',exper_name='two',session_name='2006-6-07_16-40-19',display_fn_name='DecodedEpochSlices',epochs='replays',decoder='long_results_obj',user_annotation='selections')] = np.array([5,  13,  15,  17,  20,  21,  24,  31,  33,  43,  44,  49,  63, 64,  66,  68,  70,  71,  74,  76,  77,  78,  84,  90,  94,  95, 104, 105, 122, 123])
         user_annotations[IdentifyingContext(format_name='kdiba',animal='gor01',exper_name='two',session_name='2006-6-07_16-40-19',display_fn_name='DecodedEpochSlices',epochs='replays',decoder='short_results_obj',user_annotation='selections')] = np.array([ 12,  13,  15,  17,  20,  24,  30,  31,  32,  33,  41,  43,  49, 54,  55,  68,  70,  71,  73,  76,  77,  78,  84,  89,  94, 100, 104, 105, 111, 114, 115, 117, 118, 122, 123, 131])
+
+        user_annotations[IdentifyingContext(format_name='kdiba',animal='gor01',exper_name='one',session_name='2006-6-12_15-55-31',display_fn_name='DecodedEpochSlices',epochs='replays',decoder='long_results_obj',user_annotation='selections')] = np.array([10, 11, 12, 17, 18, 22])
+        user_annotations[IdentifyingContext(format_name='kdiba',animal='gor01',exper_name='one',session_name='2006-6-12_15-55-31',display_fn_name='DecodedEpochSlices',epochs='replays',decoder='short_results_obj',user_annotation='selections')] = np.array([10, 11, 12, 16, 18, 19, 23])
+
+
 
         # IdentifyingContext(format_name='kdiba',animal='pin01',exper_name='one',session_name='11-02_17-46-44')
         user_annotations[IdentifyingContext(format_name='kdiba',animal='pin01',exper_name='one',session_name='11-02_17-46-44',display_fn_name='DecodedEpochSlices',epochs='replays',decoder='long_results_obj',user_annotation='selections')] = np.array([13, 23, 41, 46])
@@ -159,12 +198,19 @@ class UserAnnotationsManager:
         # ## TODO:
         # user_annotations[IdentifyingContext(format_name='kdiba',animal='pin01',exper_name='one',session_name='fet11-01_12-58-54',display_fn_name='DecodedEpochSlices',epochs='replays',decoder='long_results_obj',user_annotation='selections')] = np.array([])
         # user_annotations[IdentifyingContext(format_name='kdiba',animal='pin01',exper_name='one',session_name='fet11-01_12-58-54',display_fn_name='DecodedEpochSlices',epochs='replays',decoder='short_results_obj',user_annotation='selections')] = np.array([])
-        
-
-
-
-
         return user_annotations
+    
+    @classmethod
+    def has_user_annotation(cls, test_context):
+        user_anootations = cls.get_user_annotations()
+        was_annotation_found: bool = False
+        # try to find a matching user_annotation for the final_context_L
+        for a_ctx, selections_array in user_anootations.items():
+            an_item_diff = a_ctx.diff(test_context)
+            if an_item_diff == {('user_annotation', 'selections')}:
+                was_annotation_found = True
+                break # done looking
+        return was_annotation_found
 
     @staticmethod
     def update_selections_from_annotations(saved_selection, user_anootations, debug_print=True):
@@ -198,6 +244,16 @@ class UserAnnotationsManager:
         if not was_annotation_found:
             print(f'WARNING: no matching context found in {len(user_anootations)} annotations. `saved_selection` will be returned unaltered.')
         return saved_selection
+
+
+    def interactive_good_epoch_selections(self, curr_active_pipeline):
+        # Allows the user to interactively select good epochs and generate hardcoded user_annotation entries from the results:
+        ## Stacked Epoch Plot
+        example_stacked_epoch_graphics = curr_active_pipeline.display('_display_long_and_short_stacked_epoch_slices', defer_render=False, save_figure=True)
+        pagination_controller_L, pagination_controller_S = example_stacked_epoch_graphics.plot_data['controllers']
+        ax_L, ax_S = example_stacked_epoch_graphics.axes
+        final_figure_context_L, final_context_S = example_stacked_epoch_graphics.context
+
 
 
 
