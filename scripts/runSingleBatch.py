e9d5608bd41752e216803232613fb3d1b7c6acd4
Pho Hale <PhoHale@gmail.com>
2023-08-08 14:17:08 -0400

    bump

--- scripts/runSingleBatch.py
+++ scripts/runSingleBatch.py
@@ -50,9 +50,10 @@ if __name__ == "__main__":
 
     """ Usage:
     
-    python scripts/runSingleBatch.py --active_result_suffix "2023-08-08_Apogee" --num_processes 4 --should_force_reload_all --should_perform_figure_generation_to_file --debug_print
+    python scripts/runSingleBatch.py --active_result_suffix "2023-08-08_bApogee" --num_processes 4 --should_force_reload_all --debug_print
     python scripts/runSingleBatch.py --active_result_suffix "2023-08-08_LNX00052" --num_processes 4 --should_force_reload_all --debug_print
     
+    --should_perform_figure_generation_to_file
     --should_perform_figure_generation_to_file
     
     """
