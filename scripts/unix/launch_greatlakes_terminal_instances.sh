d75ded61abdeb6fe94adba0090061d8f96916bd7
Pho Hale <halechr@umich.edu>
2023-10-17 16:43:53 -0400

    GL

---
+++ scripts/unix/launch_greatlakes_terminal_instances.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# xfce4-terminal --initial-title='Neuropy' --working-directory='/home/halechr/repos/NeuroPy' --tab 
+# xfce4-terminal --initial-title='pyPhoCoreHelpers' --working-directory='/home/halechr/repos/pyPhoCoreHelpers'
+# xfce4-terminal --initial-title='pyPhoPlaceCellAnalysis' --working-directory='/home/halechr/repos/pyPhoPlaceCellAnalysis'
+# xfce4-terminal --initial-title='Spike3D' --working-directory='/home/halechr/repos/Spike3D'
+
+# xfce4-terminal --title='Neuropy' --working-directory='/home/halechr/repos/NeuroPy' --tab 
+# xfce4-terminal --title='pyPhoCoreHelpers' --working-directory='/home/halechr/repos/pyPhoCoreHelpers'
+# xfce4-terminal --title='pyPhoPlaceCellAnalysis' --working-directory='/home/halechr/repos/pyPhoPlaceCellAnalysis'
+# xfce4-terminal --title='Spike3D' --working-directory='/home/halechr/repos/Spike3D'
+
+## Good plot command:
+xfce4-terminal --tab --title='Neuropy' --working-directory='/home/halechr/repos/NeuroPy' --tab --title='pyPhoCoreHelpers' --working-directory='/home/halechr/repos/pyPhoCoreHelpers' --tab --title='pyPhoPlaceCellAnalysis' --working-directory='/home/halechr/repos/pyPhoPlaceCellAnalysis' --tab --title='Spike3D' --working-directory='/home/halechr/repos/Spike3D'
