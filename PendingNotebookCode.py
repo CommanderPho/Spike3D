89d977b3d898ceabe1d40b5b31ab5197f71a24c9
Pho Hale <PhoHale@gmail.com>
2023-11-02 13:37:31 -0400

    updating `_debug_plot_directional_template_rasters` to work with left/right separately

--- PendingNotebookCode.py
+++ PendingNotebookCode.py
@@ -49,26 +49,60 @@ def _debug_plot_directional_template_rasters(spikes_df, active_epochs_df, track_
     """
     ## spikes_df: get the spikes to plot
     # included_neuron_ids = track_templates.shared_aclus_only_neuron_IDs
-    included_neuron_ids = track_templates.shared_aclus_only_neuron_IDs
+    # included_neuron_ids = track_templates.shared_aclus_only_neuron_IDs
     # track_templates.shared_LR_aclus_only_neuron_IDs
 
+    included_neuron_ids = np.sort(np.union1d(track_templates.shared_RL_aclus_only_neuron_IDs, track_templates.shared_LR_aclus_only_neuron_IDs))
+   
+    ## add to the bottom
+    # max_idx = (np.nanmax(track_templates.shared_RL_aclus_only_neuron_IDs)-1) # -1 to convert to an index
+    
     n_neurons = len(included_neuron_ids)
 
     # Get only the spikes for the shared_aclus:
-    # spikes_df = spikes_df.spikes.sliced_by_neuron_id(included_neuron_ids)
+    spikes_df = spikes_df.spikes.sliced_by_neuron_id(included_neuron_ids)
     spikes_df = spikes_df.spikes.adding_lap_identity_column(active_epochs_df, epoch_id_key_name='new_lap_IDX')
     spikes_df = spikes_df[(spikes_df['new_lap_IDX'] != -1)] # ['lap', 'maze_relative_lap', 'maze_id']
     spikes_df, neuron_id_to_new_IDX_map = spikes_df.spikes.rebuild_fragile_linear_neuron_IDXs() # rebuild the fragile indicies afterwards
 
-    even_long, odd_long, even_short, odd_short = [(a_sort-1) for a_sort in track_templates.decoder_pf_peak_ranks_list]    
+
+    even_long, even_short = [(a_sort-1) for a_sort in track_templates.decoder_LR_pf_peak_ranks_list]
+    odd_long, odd_short = [(a_sort-1) for a_sort in track_templates.decoder_RL_pf_peak_ranks_list]
+    # even_long, odd_long, even_short, odd_short = [(a_sort-1) for a_sort in track_templates.decoder_pf_peak_ranks_list]    
     neuron_qcolors_list, neuron_colors_ndarray = DataSeriesColorHelpers.build_cell_colors(n_neurons, colormap_name='PAL-relaxed_bright', colormap_source=None)
     unit_colors_list = neuron_colors_ndarray.copy()
 
     unit_sort_orders_dict = dict(zip(['long_even', 'long_odd', 'short_even', 'short_odd'], (even_long, odd_long, even_short, odd_short)))
-    unit_colors_list_dict = dict(zip(['long_even', 'long_odd', 'short_even', 'short_odd'], (unit_colors_list, unit_colors_list, unit_colors_list, unit_colors_list)))
+    # unit_colors_list_dict = dict(zip(['long_even', 'long_odd', 'short_even', 'short_odd'], (unit_colors_list, unit_colors_list, unit_colors_list, unit_colors_list)))
 
-    app, win, plots, plots_data, on_update_active_epoch, on_update_active_scatterplot_kwargs = _plot_multi_sort_raster_browser(spikes_df, included_neuron_ids, unit_sort_orders_dict=unit_sort_orders_dict, unit_colors_list_dict=unit_colors_list_dict, scatter_app_name='pho_directional_laps_rasters', defer_show=False, active_context=None)
-    return app, win, plots, plots_data, (on_update_active_epoch, on_update_active_scatterplot_kwargs)
+    
+    ## Do Even/Odd Separately:
+    unit_colors_map = dict(zip(included_neuron_ids, neuron_colors_ndarray.copy().T))
+    odd_unit_colors_list = np.array([v for k, v in unit_colors_map.items() if k in track_templates.shared_RL_aclus_only_neuron_IDs]).T # should be (4, len(shared_RL_aclus_only_neuron_IDs))
+    even_unit_colors_list = np.array([v for k, v in unit_colors_map.items() if k in track_templates.shared_LR_aclus_only_neuron_IDs]).T # should be (4, len(shared_RL_aclus_only_neuron_IDs))
+    unit_colors_list_dict = dict(zip(['long_even', 'long_odd', 'short_even', 'short_odd'], (even_unit_colors_list, odd_unit_colors_list, even_unit_colors_list, odd_unit_colors_list)))
+
+
+    # Odd:
+    odd_names = ['long_odd', 'short_odd']
+    odd_unit_sort_orders_dict = {k:v for k, v in unit_sort_orders_dict.items() if k in odd_names}
+    odd_unit_colors_list_dict = {k:v for k, v in unit_colors_list_dict.items() if k in odd_names}
+    odd_spikes_df = deepcopy(spikes_df).spikes.sliced_by_neuron_id(track_templates.shared_RL_aclus_only_neuron_IDs)
+    odd_spikes_df, odd_neuron_id_to_new_IDX_map = odd_spikes_df.spikes.rebuild_fragile_linear_neuron_IDXs() # rebuild the fragile indicies afterwards
+    odd_display_outputs = _plot_multi_sort_raster_browser(odd_spikes_df, track_templates.shared_RL_aclus_only_neuron_IDs, unit_sort_orders_dict=odd_unit_sort_orders_dict, unit_colors_list_dict=odd_unit_colors_list_dict, scatter_app_name='pho_directional_laps_rasters_ODD', defer_show=False, active_context=None)
+    # app, win, plots, plots_data, on_update_active_epoch, on_update_active_scatterplot_kwargs = odd_display_outputs
+    
+    # Even:
+    even_names = ['long_even', 'short_even']
+    even_unit_sort_orders_dict = {k:v for k, v in unit_sort_orders_dict.items() if k in even_names}
+    even_unit_colors_list_dict = {k:v for k, v in unit_colors_list_dict.items() if k in even_names}
+    even_spikes_df = deepcopy(spikes_df).spikes.sliced_by_neuron_id(track_templates.shared_LR_aclus_only_neuron_IDs)
+    even_spikes_df, even_neuron_id_to_new_IDX_map = even_spikes_df.spikes.rebuild_fragile_linear_neuron_IDXs() # rebuild the fragile indicies afterwards
+    even_display_outputs = _plot_multi_sort_raster_browser(even_spikes_df, track_templates.shared_LR_aclus_only_neuron_IDs, unit_sort_orders_dict=even_unit_sort_orders_dict, unit_colors_list_dict=even_unit_colors_list_dict, scatter_app_name='pho_directional_laps_rasters_EVEN', defer_show=False, active_context=None)
+    # app, win, plots, plots_data, on_update_active_epoch, on_update_active_scatterplot_kwargs = even_display_outputs
+
+    # app, win, plots, plots_data, on_update_active_epoch, on_update_active_scatterplot_kwargs = _plot_multi_sort_raster_browser(spikes_df, included_neuron_ids, unit_sort_orders_dict=unit_sort_orders_dict, unit_colors_list_dict=unit_colors_list_dict, scatter_app_name='pho_directional_laps_rasters', defer_show=False, active_context=None)
+    return odd_display_outputs, even_display_outputs
 
 
 from scipy import stats # _recover_samples_per_sec_from_laps_df
