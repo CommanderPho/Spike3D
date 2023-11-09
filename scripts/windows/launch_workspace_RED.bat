7a1651d069ab6f86115d6e6d28ddff7b4ac19ba3
Pho Hale <PhoHale@gmail.com>
2023-11-09 01:53:36 -0500

    11-8

---
+++ scripts/windows/launch_workspace_RED.bat
@@ -0,0 +1,6 @@
+@echo off
+echo launch_workspace_RED.bat
+code --user-data-dir="K:\FastSwap\AppData\CodeRed"  --extensions-dir="K:\FastSwap\AppData\VSCode\red\extensions" "C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\EXTERNAL\VSCode_Workspaces\PhoPy3DPositionAnalysis_PhoLibs_workspace_WINDOW_RED.code-workspace"
+echo "done"
+pause
+
