3a1dac8dedc6d56155a2fcacf0bc8fbbc4a3ff23
Pho Hale <PhoHale@gmail.com>
2023-12-23 15:53:27 -0500

    bump

---
+++ EXTERNAL/DEVELOPER_NOTES/2023-12-23-PicklingError.py
@@ -0,0 +1,31 @@
+INFO:dill:# M2
+M2: <module 'types' from '/home/halechr/.pyenv/versions/3.9.13/lib/python3.9/types.py'>
+INFO:dill:M2: <module 'types' from '/home/halechr/.pyenv/versions/3.9.13/lib/python3.9/types.py'>
+# M2
+INFO:dill:# M2
+M2: <module 'typing' from '/home/halechr/.pyenv/versions/3.9.13/lib/python3.9/typing.py'>
+INFO:dill:M2: <module 'typing' from '/home/halechr/.pyenv/versions/3.9.13/lib/python3.9/typing.py'>
+# M2
+INFO:dill:# M2
+M2: <module 'attr._compat' from '/home/halechr/repos/Spike3D/.venv/lib/python3.9/site-packages/attr/_compat.py'>
+INFO:dill:M2: <module 'attr._compat' from '/home/halechr/repos/Spike3D/.venv/lib/python3.9/site-packages/attr/_compat.py'>
+# M2
+INFO:dill:# M2
+M2: <module 'attr._config' from '/home/halechr/repos/Spike3D/.venv/lib/python3.9/site-packages/attr/_config.py'>
+INFO:dill:M2: <module 'attr._config' from '/home/halechr/repos/Spike3D/.venv/lib/python3.9/site-packages/attr/_config.py'>
+# M2
+INFO:dill:# M2
+M2: <module 'attr.setters' from '/home/halechr/repos/Spike3D/.venv/lib/python3.9/site-packages/attr/setters.py'>
+INFO:dill:M2: <module 'attr.setters' from '/home/halechr/repos/Spike3D/.venv/lib/python3.9/site-packages/attr/setters.py'>
+# M2
+INFO:dill:# M2
+T4: <class 'attr._compat._AnnotationExtractor'>
+INFO:dill:T4: <class 'attr._compat._AnnotationExtractor'>
+# T4
+INFO:dill:# T4
+F2: <function make_set_closure_cell.<locals>.set_closure_cell at 0x7f016b85ab80>
+INFO:dill:F2: <function make_set_closure_cell.<locals>.set_closure_cell at 0x7f016b85ab80>
+done.
+Saving (file mode '/media/MAX/Data/KDIBA/gor01/one/2006-6-08_14-26-15/output/2023-12-23_158pm-minimum_inclusion_fr-5-included_qclu_values-[1, 2]RankOrder.pkl') saved session pickle file results : /media/MAX/Data/KDIBA/gor01/one/2006-6-08_14-26-15/output/2023-12-23_158pm-minimum_inclusion_fr-5-included_qclu_values-[1, 2]RankOrder.pkl... pickling exception occured while using safeSaveData(pkl_path: /media/MAX/Data/KDIBA/gor01/one/2006-6-08_14-26-15/output/2023-12-23_158pm-minimum_inclusion_fr-5-included_qclu_values-[1, 2]RankOrder.pkl, ..., , should_append=False) but original file was NOT overwritten!
+Exception: Can't pickle <function make_set_closure_cell.<locals>.set_closure_cell at 0x7f016b85ab80>: it's not found as attr._compat.make_set_closure_cell.<locals>.set_closure_cell
+done.
