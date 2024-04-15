7d5eda35bf0068016d9f5bbaa503f7ffefa78b4b
Pho Hale <halechr@umich.edu>
2024-04-15 10:29:51 -0400

    bump

--- scripts/unix/launch_jupyter_lab_with_remote_access.sh
+++ scripts/unix/launch_jupyter_lab_with_remote_access.sh
@@ -2,4 +2,4 @@
 cd /home/halechr/repos/Spike3D
 ./scripts/unix/repos_pull_changes.sh
 poetry shell
-jupyter-lab --no-browser --port=8889 --ip=0.0.0.0
+jupyter-lab --no-browser --port=8889 --NotebookApp.ip='0.0.0.0' --NotebookApp.allow_origin='*'
