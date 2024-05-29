4821d321d177a4ce9ee595c19079b203c40625d8
Pho Hale <PhoHale@gmail.com>
2024-05-29 15:56:45 -0400

    fixed launch_jupyter_lab_with_remote_access.sh resetting on poetry shell

--- scripts/unix/launch_jupyter_lab_with_remote_access.sh
+++ scripts/unix/launch_jupyter_lab_with_remote_access.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 
 cd /home/halechr/repos/Spike3D
 ./scripts/unix/repos_pull_changes.sh
-poetry shell
+# Get the path to the Poetry virtual environment
+VENV_PATH=$(poetry env info --path)
+
+# Source the activate script of the virtual environment
+source $VENV_PATH/bin/activate
+# poetry shell
+
+
 jupyter-lab --no-browser --port=8889 --NotebookApp.ip='0.0.0.0' --NotebookApp.allow_origin='*'
