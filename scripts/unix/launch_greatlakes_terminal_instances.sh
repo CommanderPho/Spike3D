5db6575830a23c52311e36d4b2024aef5aae0a6c
Pho Hale <PhoHale@gmail.com>
2024-05-31 11:35:19 -0400

    b

--- scripts/unix/launch_greatlakes_terminal_instances.sh
+++ scripts/unix/launch_greatlakes_terminal_instances.sh
@@ -11,12 +11,19 @@ xfce4-terminal \
 --tab --title='pyPhoPlaceCellAnalysis' --working-directory='/home/halechr/repos/pyPhoPlaceCellAnalysis' --command="bash -c '$cmd_git_ops'" \
 --tab --title='Spike3D' --working-directory='/home/halechr/repos/Spike3D' --command="bash -c '$cmd_git_ops'" &
 
+# Ensure the JupyterLab window opens immediately
+sleep 1
+
 # Launch Visual Studio Code separately
 /home/halechr/bin/VSCode-linux-x64/bin/code &
+
+# Wait a bit to make sure the VS Code command does not interfere with the terminal launch
+sleep 1
 
 # Launch a second xfce4-terminal window and execute the JupyterLab launch script
 xfce4-terminal \
 --window \
 --title='Jupyter Lab' \
---working-directory='/home/halechr/repos/Spike3D' \
---command="/home/halechr/repos/Spike3D/scripts/unix/launch_jupyter_lab_with_remote_access.sh" &
+--working-directory='/home/halechr/repos' \
+--command="bash /home/halechr/repos/scripts/unix/launch_jupyter_lab_with_remote_access.sh" &
+
