2ec20b866b40ad067b29d67e9cdcb2f737111d96
Pho Hale <PhoHale@gmail.com>
2024-05-31 11:25:44 -0400

    GL script

--- scripts/unix/launch_greatlakes_terminal_instances.sh
+++ scripts/unix/launch_greatlakes_terminal_instances.sh
@@ -11,5 +11,12 @@
 # xfce4-terminal --title='Spike3D' --working-directory='/home/halechr/repos/Spike3D'
 
 ## Launches all tabs in a single terminal:
-xfce4-terminal --window --title='Neuropy' --working-directory='/home/halechr/repos/NeuroPy' --tab --title='pyPhoCoreHelpers' --working-directory='/home/halechr/repos/pyPhoCoreHelpers' --tab --title='pyPhoPlaceCellAnalysis' --working-directory='/home/halechr/repos/pyPhoPlaceCellAnalysis' --tab --title='Spike3D' --working-directory='/home/halechr/repos/Spike3D'
+# xfce4-terminal --window --title='Neuropy' --working-directory='/home/halechr/repos/NeuroPy' --tab --title='pyPhoCoreHelpers' --working-directory='/home/halechr/repos/pyPhoCoreHelpers' --tab --title='pyPhoPlaceCellAnalysis' --working-directory='/home/halechr/repos/pyPhoPlaceCellAnalysis' --tab --title='Spike3D' --working-directory='/home/halechr/repos/Spike3D'
 
+#!/bin/bash
+
+# Launches all tabs in a single terminal and runs git pull followed by git status in each tab
+xfce4-terminal --window --title='Neuropy' --working-directory='/home/halechr/repos/NeuroPy' -e 'bash -c "git pull && git status; exec bash"' \
+--tab --title='pyPhoCoreHelpers' --working-directory='/home/halechr/repos/pyPhoCoreHelpers' -e 'bash -c "git pull && git status; exec bash"' \
+--tab --title='pyPhoPlaceCellAnalysis' --working-directory='/home/halechr/repos/pyPhoPlaceCellAnalysis' -e 'bash -c "git pull && git status; exec bash"' \
+--tab --title='Spike3D' --working-directory='/home/halechr/repos/Spike3D' -e 'bash -c "git pull && git status; exec bash"'
