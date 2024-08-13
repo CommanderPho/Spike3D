99e6ebc705dd5f3254a8323c251852e504da7766
Pho Hale <PhoHale@gmail.com>
2024-08-13 01:24:13 -0400

    bump

--- scripts/windows/display_pyqtplot_examples.bat
+++ scripts/windows/display_pyqtplot_examples.bat
@@ -1,10 +1,12 @@
 @echo off
-echo display_pyqtplot_examples.bat
+echo "Running display_pyqtplot_examples.bat..."
 @REM call C:/Users/pho/miniconda3/Scripts/activate
 @REM conda activate phoviz_ultimate
 @REM START /B "C:/Users/pho/miniconda3/envs/phoviz_ultimate/python.exe" "c:/Users/pho/repos/PhoPy3DPositionAnalysis2021/PyQtPlotExamples/PyQtPlot_EXAMPLES.py"
 @REM "C:/Users/pho/miniconda3/envs/phoviz_ultimate/python.exe" "c:/Users/pho/repos/PhoPy3DPositionAnalysis2021/PyQtPlotExamples/PyQtPlot_EXAMPLES.py"
-"C:/Users/pho/AppData/Local/pypoetry/Cache/virtualenvs/spike3d-WtdfU5rp-py3.9/Scripts/python.exe" "../LibrariesExamples/PyQtPlot/PyQtPlot_EXAMPLES.py"
+@REM "C:/Users/pho/AppData/Local/pypoetry/Cache/virtualenvs/spike3d-WtdfU5rp-py3.9/Scripts/python.exe" "../LibrariesExamples/PyQtPlot/PyQtPlot_EXAMPLES.py"
+@REM python "C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\LibrariesExamples\PyQtPlot\PyQtPlot_EXAMPLES.py"
+"C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\.venv\Scripts\python.exe" "C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\LibrariesExamples\PyQtPlot\PyQtPlot_EXAMPLES.py"
 echo "done"
 pause
 
