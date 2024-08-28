1bb6f8681d0065d63a5aba9b211a1ac9a7d72359
Pho Hale <halechr@umich.edu>
2024-08-28 08:09:56 -0400

    bump

--- scripts/unix/greatlakes_relocate_poetry_venvs.sh
+++ scripts/unix/greatlakes_relocate_poetry_venvs.sh
@@ -21,6 +21,15 @@ mv '/home/halechr/repos/pyPhoPlaceCellAnalysis/.venv' '/home/halechr/cloud/turbo
 ln -s '/home/halechr/cloud/turbo/Pho/Environments/pyPhoPlaceCellAnalysis_venv' '/home/halechr/repos/pyPhoPlaceCellAnalysis/.venv'
 
 
+mv '/home/halechr/repos/Spike3D/.venv' '/home/halechr/repos/Spike3D/Spike3D_venv'
+
+
+mv '/home/halechr/repos/Spike3D/Spike3D_venv' '/home/halechr/cloud/turbo/Pho/Environments/Spike3D_venv'
+
+
+mv '/home/halechr/repos/Spike3D/.venv' '/home/halechr/cloud/turbo/Pho/Environments/Spike3D_venv'
+ln -s '/home/halechr/cloud/turbo/Pho/Environments/Spike3D_venv' '/home/halechr/repos/Spike3D/.venv'
+
 
 # ```bash
 # bash-4.4$ poetry env list --full-path
@@ -41,4 +50,25 @@ ln -s '/home/halechr/cloud/turbo/Pho/Environments/pyPhoPlaceCellAnalysis_venv' '
 # Path:       /sw/pkgs/arc/python/3.9.12
 # Executable: /sw/pkgs/arc/python/3.9.12/bin/python3.9
 
+# ```
+# /home/halechr/repos/Spike3D/.venv
+
+# ```bash
+# bash-4.4$ poetry env list --full-path
+# /home/halechr/repos/Spike3D/.venv (Activated)
+# bash-4.4$ poetry env info
+
+# Virtualenv
+# Python:         3.9.12
+# Implementation: CPython
+# Path:           /home/halechr/repos/Spike3D/.venv
+# Executable:     /home/halechr/repos/Spike3D/.venv/bin/python
+# Valid:          True
+
+# System
+# Platform:   linux
+# OS:         posix
+# Python:     3.9.12
+# Path:       /sw/pkgs/arc/python/3.9.12
+# Executable: /sw/pkgs/arc/python/3.9.12/bin/python3.9
 # ```
