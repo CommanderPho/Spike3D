5b803d6f57533f11608c0d257f4f8b7aa7190d58
Pho Hale <halechr@umich.edu>
2024-08-28 07:10:09 -0400

    GL

---
+++ scripts/unix/greatlakes_relocate_poetry_venvs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## Moves the individual poetry .venvs to the turbo/Pho directory on Greatlakes
+
+
+# cp -R '/home/halechr/repos/NeuroPy/.venv' '/home/halechr/cloud/turbo/Pho/Environments/NeuroPy_venv'
+
+# cp -R '/home/halechr/repos/NeuroPy/.venv' '/home/halechr/cloud/turbo/Pho/Environments/NeuroPy_venv'
+# rm -rf '/home/halechr/repos/NeuroPy/.venv'
+
+mv '/home/halechr/repos/NeuroPy/.venv' '/home/halechr/cloud/turbo/Pho/Environments/NeuroPy_venv'
+ln -s '/home/halechr/cloud/turbo/Pho/Environments/NeuroPy_venv' '/home/halechr/repos/NeuroPy/.venv'
+
+
+mv '/home/halechr/repos/pyPhoCoreHelpers/.venv' '/home/halechr/cloud/turbo/Pho/Environments/pyPhoCoreHelpers_venv'
+ln -s '/home/halechr/cloud/turbo/Pho/Environments/pyPhoCoreHelpers_venv' '/home/halechr/repos/pyPhoCoreHelpers/.venv'
+
+# poetry env use '/home/halechr/cloud/turbo/Pho/Environments/pyPhoCoreHelpers_venv/bin/python' # NOT NEEDED
+
+mv '/home/halechr/repos/pyPhoPlaceCellAnalysis/.venv' '/home/halechr/cloud/turbo/Pho/Environments/pyPhoPlaceCellAnalysis_venv'
+ln -s '/home/halechr/cloud/turbo/Pho/Environments/pyPhoPlaceCellAnalysis_venv' '/home/halechr/repos/pyPhoPlaceCellAnalysis/.venv'
+
+
+
+# ```bash
+# bash-4.4$ poetry env list --full-path
+# /home/halechr/repos/pyPhoCoreHelpers/.venv (Activated)
+# bash-4.4$ poetry env info
+
+# Virtualenv
+# Python:         3.9.12
+# Implementation: CPython
+# Path:           /home/halechr/repos/pyPhoCoreHelpers/.venv
+# Executable:     /home/halechr/repos/pyPhoCoreHelpers/.venv/bin/python
+# Valid:          True
+
+# System
+# Platform:   linux
+# OS:         posix
+# Python:     3.9.12
+# Path:       /sw/pkgs/arc/python/3.9.12
+# Executable: /sw/pkgs/arc/python/3.9.12/bin/python3.9
+
+# ```
