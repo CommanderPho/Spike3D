9e418668ea637c04d710b3d529527a05063bcc28
Pho Hale <PhoHale@gmail.com>
2024-08-30 17:58:48 -0400

    bump

--- scripts/unix/export_repo_requirements_txt.sh
+++ scripts/unix/export_repo_requirements_txt.sh
@@ -2,36 +2,4 @@
 
 # Exports a requirements.txt suitable for use by pip/virtualenv from each repo's poetry project:
 
-# Define directories
-spike3DDirectory=$(dirname $(dirname "$BASH_SOURCE"))
-repoParentDirectory=$(dirname "$spike3DDirectory")
-
-directories=(
-    "$repoParentDirectory/NeuroPy"
-    "$repoParentDirectory/pyPhoCoreHelpers"
-    "$repoParentDirectory/pyPhoPlaceCellAnalysis"
-    "$spike3DDirectory"
-)
-
-# Commands to run in each directory
-commands=(
-    "poetry export --without-hashes --format=requirements.txt > requirements.txt"
-    "poetry export --without-hashes --format=requirements.txt > requirements.txt"
-    "poetry export --without-hashes --format=requirements.txt > requirements.txt"
-    "poetry export --without-hashes --format=requirements.txt > requirements.txt"
-)
-
-# Function to export requirements
-export_requirements_from_poetry() {
-    echo "Exporting requirements.txt from poetry repos..."
-    for i in "${!directories[@]}"; do
-        directory="${directories[i]}"
-        command="${commands[i]}"
-        echo "Running in $directory"
-        cd "$directory" || exit
-        eval "$command"
-        echo "done."
-    done
-}
-
-export_requirements_from_poetry
+& python ../helpers/export_subrepos.py
