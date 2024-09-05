5e396cecb1651fbef2356695d58be6e4485a3cdc
Pho Hale <PhoHale@gmail.com>
2024-09-05 08:23:34 -0400

    09-05 to lab

--- scripts/open_generated_jupyter_book_docs.py
+++ scripts/open_generated_jupyter_book_docs.py
@@ -3,4 +3,4 @@ import os
 
 # Replace 'your_file_path.html' with the actual path to your HTML file
 file_path = os.path.abspath('EXTERNAL\PhoDibaPaper2024Book\_build\html\index.html')
-webbrowser.open(f'file://{file_path}')
+webbrowser.open(f'file://{file_path}', new=0)
