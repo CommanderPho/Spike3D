15588ff25fbf560b0eb2e0f394fe0a11622d3ab8
Pho Hale <halechr@umich.edu>
2024-10-01 08:14:38 -0400

    GL

--- scripts/unix/launch_jupyter_lab_with_remote_access.sh
+++ scripts/unix/launch_jupyter_lab_with_remote_access.sh
@@ -3,8 +3,9 @@
 cd /home/halechr/repos/Spike3D
 ./scripts/unix/repos_pull_changes.sh
 # Get the path to the Poetry virtual environment
-VENV_PATH=$(poetry env info --path)
-
+# VENV_PATH=$(poetry env info --path)
+VENV_PATH="/home/halechr/Library/VSCode/green/.venv_green"
+# /home/halechr/Library/VSCode/green/.venv_green/bin/python
 # Source the activate script of the virtual environment
 source $VENV_PATH/bin/activate
 # poetry shell
