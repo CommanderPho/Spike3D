e88b5fe9a3de18055bd51e336df2b86ac1571a36
Pho Hale <halechr@umich.edu>
2024-10-08 16:23:49 -0400

    changed back to poetry env

--- scripts/unix/launch_jupyter_lab_with_remote_access.sh
+++ scripts/unix/launch_jupyter_lab_with_remote_access.sh
@@ -3,13 +3,12 @@
 cd /home/halechr/repos/Spike3D
 ./scripts/unix/repos_pull_changes.sh
 # Get the path to the Poetry virtual environment
-# VENV_PATH=$(poetry env info --path)
-VENV_PATH="/home/halechr/Library/VSCode/green/.venv_green"
+VENV_PATH=$(poetry env info --path)
+# VENV_PATH="/home/halechr/Library/VSCode/green/.venv_green"
 # /home/halechr/Library/VSCode/green/.venv_green/bin/python
 # Source the activate script of the virtual environment
-source $VENV_PATH/bin/activate
-# poetry shell
-
+# source $VENV_PATH/bin/activate
+poetry shell
 
 jupyter-lab --no-browser --port=8889 --NotebookApp.ip='0.0.0.0' --NotebookApp.allow_origin='*'
 
