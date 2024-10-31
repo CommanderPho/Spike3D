68f6e25158a2343c2494c35625b8f73c9e4b5f57
Pho Hale <PhoHale@gmail.com>
2024-10-31 07:51:51 -0400

    fix jupyter lab launch script

--- scripts/unix/launch_jupyter_lab_with_remote_access.sh
+++ scripts/unix/launch_jupyter_lab_with_remote_access.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 cd /home/halechr/repos/Spike3D
-./scripts/unix/repos_pull_changes.sh
+# ./scripts/unix/repos_pull_changes.sh
+
+# Ensure Poetry is available (optional but recommended)
+if ! command -v poetry &> /dev/null
+then
+    echo "Poetry could not be found. Please install Poetry first."
+    exit 1
+fi
+
 # Get the path to the Poetry virtual environment
 VENV_PATH=$(poetry env info --path)
 # VENV_PATH="/home/halechr/Library/VSCode/green/.venv_green"
 # /home/halechr/Library/VSCode/green/.venv_green/bin/python
 # Source the activate script of the virtual environment
 # source $VENV_PATH/bin/activate
-poetry shell
+# poetry shell
 
-jupyter-lab --no-browser --port=8889 --NotebookApp.ip='0.0.0.0' --NotebookApp.allow_origin='*'
-
+# Run Jupyter Lab within the Poetry environment
+poetry run jupyter-lab --no-browser --port=8889 --NotebookApp.ip='0.0.0.0' --NotebookApp.allow_origin='*'
