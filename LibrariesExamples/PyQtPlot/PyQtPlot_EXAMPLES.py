7f00d542f129ccfae5f4954e097b5108b84f5ce3
Pho Hale <PhoHale@gmail.com>
2025-03-08 16:59:13 -0500

    ExampleApp setting default Qt lib

--- LibrariesExamples/PyQtPlot/PyQtPlot_EXAMPLES.py
+++ LibrariesExamples/PyQtPlot/PyQtPlot_EXAMPLES.py
@@ -1,3 +1,8 @@
+# Set PyQt5 as the preferred binding before importing pyqtgraph
+import os
+os.environ['PYQTGRAPH_QT_LIB'] = 'PyQt5'
+
+# Now import pyqtgraph
 import pyphoplacecellanalysis.External.pyqtgraph as pg
 # import pyphoplacecellanalysis.External.pyqtgraph.examples
 from pyphoplacecellanalysis.External.pyqtgraph import examples
