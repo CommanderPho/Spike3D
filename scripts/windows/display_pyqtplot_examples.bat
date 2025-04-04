b46f27dfc30fceba97e4856e7e60ceee9a13fc12
Pho Hale <PhoHale@gmail.com>
2025-04-04 07:17:18 -0400

    PyQtGraph Examples

--- scripts/windows/display_pyqtplot_examples.bat
+++ scripts/windows/display_pyqtplot_examples.bat
@@ -6,7 +6,8 @@ echo "Running display_pyqtplot_examples.bat..."
 @REM "C:/Users/pho/miniconda3/envs/phoviz_ultimate/python.exe" "c:/Users/pho/repos/PhoPy3DPositionAnalysis2021/PyQtPlotExamples/PyQtPlot_EXAMPLES.py"
 @REM "C:/Users/pho/AppData/Local/pypoetry/Cache/virtualenvs/spike3d-WtdfU5rp-py3.9/Scripts/python.exe" "../LibrariesExamples/PyQtPlot/PyQtPlot_EXAMPLES.py"
 @REM python "C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\LibrariesExamples\PyQtPlot\PyQtPlot_EXAMPLES.py"
-"C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\.venv\Scripts\python.exe" "C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\LibrariesExamples\PyQtPlot\PyQtPlot_EXAMPLES.py"
+@REM "C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\.venv\Scripts\python.exe" "C:\Users\pho\repos\Spike3DWorkEnv\Spike3D\LibrariesExamples\PyQtPlot\PyQtPlot_EXAMPLES.py"
+poetry run python "LibrariesExamples\PyQtPlot\PyQtPlot_EXAMPLES.py"
 echo "done"
 pause
 
