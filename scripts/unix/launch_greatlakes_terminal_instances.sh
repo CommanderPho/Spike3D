21cbd3210c0c357e1c13f3ed5f9cc53ad9f7cbdb
Pho Hale <halechr@umich.edu>
2025-08-21 13:11:36 -0400

    GL

--- scripts/unix/launch_greatlakes_terminal_instances.sh
+++ scripts/unix/launch_greatlakes_terminal_instances.sh
@@ -10,20 +10,8 @@ geometry1="900x30+0+0"
 xfce4-terminal \
 --window \
 --geometry=$geometry1 \
---tab --title='Spike3D' --working-directory='$HOME/repos/Spike3D' --command="bash -c '$cmd_git_ops'" &
-# --tab --title='Spike3D UV' --working-directory='$HOME/repos/Spike3D' --command="bash -c '$cmd_git_ops; source .UV_venv/bin/activate; ipython ProcessBatchOutputs_qclus1246789_Only.ipy'" &
+--tab --title='Spike3D .venv' --working-directory='$HOME/repos/Spike3D' --command="bash -c 'source .venv/bin/activate; exec bash'" &
 
-
-# Launch Visual Studio Code separately
+Launch Visual Studio Code separately
 $HOME/bin/VSCode-linux-x64/bin/code &
 
-# # Geometry for the Jupyter terminal (e.g., 90 columns wide by 30 lines tall, offset by 100 pixels to the right and down)
-# geometry2="90x30+100+100"
-
-# # Launch a second xfce4-terminal window and execute the JupyterLab launch script
-# xfce4-terminal \
-# --window \
-# --geometry=$geometry2 \
-# --title='Jupyter Lab' \
-# --working-directory='$HOME/repos' \
-# --command="bash $HOME/repos/scripts/unix/launch_jupyter_lab_with_remote_access.sh" &
