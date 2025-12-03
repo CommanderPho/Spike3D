60e96045f08db50f5c728da102880e423a0aee6d
Pho Hale <halechr@umich.edu>
2025-12-03 09:49:50 -0500

    improved readibility of output

--- scripts/unix/repos_pull_changes.sh
+++ scripts/unix/repos_pull_changes.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 cd ~/repos/NeuroPy/
+echo "============= NeuroPy:"
 git pull
 cd ~/repos/pyPhoCoreHelpers/
+echo "============= pyPhoCoreHelpers:"
 git pull
 cd ~/repos/pyPhoPlaceCellAnalysis/
+echo "============= pyPhoPlaceCellAnalysis:"
 git pull
 cd ~/repos/Spike3D/
+echo "============= Spike3D:"
 git pull
+echo "done with all."
